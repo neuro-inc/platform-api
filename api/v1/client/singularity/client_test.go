e4c87a2732b51b7e9c67072ff41f81a3ccedc870
hagen1778 <hagen1778@gmail.com>
2018-04-06 16:35:01 +0300

    add internal; job.Start() test

--- api/v1/client/singularity/client_test.go
+++ api/v1/client/singularity/client_test.go
@@ -1,11 +1,16 @@
 package singularity
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
+	"sync"
 	"testing"
 	"time"
+
+	"github.com/neuromation/platform-api/api/v1/container"
 )
 
 func TestNewClient_Fail(t *testing.T) {
@@ -86,15 +91,27 @@ func TestGetPost(t *testing.T) {
 				t.Fatalf("expected to get err: %s; got instead: %q", tc.expectedErr, gotErr)
 			}
 		}
-		t.Run("POST" + tc.name, func(t *testing.T) {
+		t.Run("POST"+tc.name, func(t *testing.T) {
 			_, err := sc.post(tc.addr, "{}")
 			checkErr(err)
 		})
-		t.Run("GET" + tc.name, func(t *testing.T) {
+		t.Run("GET"+tc.name, func(t *testing.T) {
 			_, err := sc.get(tc.addr)
 			checkErr(err)
 		})
 	}
+}
+
+func TestSingularityJob_Start2(t *testing.T) {
+	sc := fakeClient(t)
+	job := sc.NewJob(container.Container{}, container.Resources{})
+	if err := job.Start(); err != nil {
+		t.Fatalf("unexpected job.Start() error: %s", err)
+	}
+}
+
+var serverState = &singularityState{
+	r: make(map[string]struct{}),
 }
 
 func singularityHandler(rw http.ResponseWriter, r *http.Request) {
@@ -105,7 +122,61 @@ func singularityHandler(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(http.StatusInternalServerError)
 	case "/singularity/api/test/timeout":
 		time.Sleep(time.Second * 5)
+	case "/singularity/api/deploys":
+		if r.Method != "POST" {
+			respondWith(rw, "non-POST request received", http.StatusBadRequest)
+			return
+		}
+		decoder := json.NewDecoder(r.Body)
+		deploy := &singularityJob{}
+		err := decoder.Decode(deploy)
+		if err != nil {
+			respondWith(rw, "unable to decode deploy payload", http.StatusBadRequest)
+			return
+		}
+		r.Body.Close()
+		if len(deploy.Deploy.ID) < 1 {
+			respondWith(rw, "got empty deploy ID", http.StatusBadRequest)
+			return
+		}
+		if !serverState.get(deploy.Deploy.RequestID) {
+			respondWith(rw, "no RequestID in server state. Probably /deploys was called before /requests", http.StatusBadRequest)
+			return
+		}
+	case "/singularity/api/requests":
+		if r.Method != "POST" {
+			respondWith(rw, "non-POST request received", http.StatusBadRequest)
+			return
+		}
+		decoder := json.NewDecoder(r.Body)
+		request := &struct {
+			ID          string `json:"id"`
+			RequestType string `json:"requestType"`
+		}{}
+		err := decoder.Decode(request)
+		if err != nil {
+			respondWith(rw, "unable to decode request payload", http.StatusBadRequest)
+			return
+		}
+		r.Body.Close()
+		if len(request.ID) < 1 {
+			respondWith(rw, "got empty request ID", http.StatusBadRequest)
+			return
+		}
+		serverState.set(request.ID)
+		expType := "RUN_ONCE"
+		if request.RequestType != expType {
+			respondWith(rw, fmt.Sprintf("got requestType %q; expected to be %q", request.RequestType, expType), http.StatusBadRequest)
+			return
+		}
+	default:
+		respondWith(rw, fmt.Sprintf("unsupported path: %s", r.URL.Path), http.StatusBadRequest)
 	}
+}
+
+func respondWith(rw http.ResponseWriter, msg string, sc int) {
+	rw.WriteHeader(sc)
+	fmt.Fprint(rw, msg)
 }
 
 func fakeClient(t *testing.T) *singularityClient {
@@ -116,4 +187,23 @@ func fakeClient(t *testing.T) *singularityClient {
 		t.Fatalf("expected to create client successfully; got err: %s", err)
 	}
 	return c.(*singularityClient)
+}
+
+type singularityState struct {
+	sync.Mutex
+	// r is a registry of requests, where key is unique id of received request
+	r map[string]struct{}
+}
+
+func (r *singularityState) set(key string) {
+	r.Lock()
+	r.r[key] = struct{}{}
+	r.Unlock()
+}
+
+func (r *singularityState) get(key string) bool {
+	r.Lock()
+	defer r.Unlock()
+	_, ok := r.r[key]
+	return ok
 }
