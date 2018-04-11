66b67649d59e22f58c4e6a1d7dabe08cbc4b9393
Aleksandr Danshyn <dalazx@gmail.com>
2018-04-11 15:38:36 +0300

    Added StatusService

--- api/v1/status.go
+++ api/v1/status.go
@@ -1,5 +1,9 @@
 package v1
 
+import (
+	"github.com/satori/go.uuid"
+)
+
 
 // TODO: implement status service that stores ID mapping in-memory
 
@@ -22,9 +26,51 @@ func (name StatusName) String() string {
 }
 
 type Status struct {
-	Id int
+	Id string
 	Status StatusName
 }
 
-func getStatus(id string) {
+
+type StatusService interface {
+	Create() Status
+	Get(id string) Status
+	Update() Status
+	Delete(id string) Status
+}
+
+
+type InMemoryStatusService struct {
+	statuses map[string]Status
+}
+
+func NewInMemoryStatusService() *InMemoryStatusService {
+	service := new(InMemoryStatusService)
+	service.statuses = make(map[string]Status)
+	return service
+}
+
+func (service *InMemoryStatusService) Create() Status {
+	id := uuid.Must(uuid.NewV4()).String()
+	status := Status{
+		Id: id,
+		Status: STATUS_PENDING,
+	}
+	return status
+}
+
+func (service *InMemoryStatusService) Get(id string) Status {
+	return Status{}
+}
+
+func (service *InMemoryStatusService) Update() Status {
+	return Status{}
+}
+
+func (service *InMemoryStatusService) Delete(id string) Status {
+	return Status{}
+}
+
+func NewStatusService() StatusService {
+	service := NewInMemoryStatusService()
+	return service
 }
--- api/v1/status_test.go
+++ api/v1/status_test.go
@@ -11,3 +11,16 @@ func TestStatusNameString(t *testing.T) {
 		t.Fatal()
 	}
 }
+
+func TestInMemoryStatusServiceCreate(t *testing.T) {
+	service := NewInMemoryStatusService()
+	status := service.Create()
+
+	if len(status.Id) != 36 {
+		t.Fatal()
+	}
+
+	if status.Status != STATUS_PENDING {
+		t.Fatal()
+	}
+}
