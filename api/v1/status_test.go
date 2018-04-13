cb8b33a7094d119996c104ef36aa66eeeae5169b
Aleksandr Danshyn <dalazx@gmail.com>
2018-04-13 18:58:00 +0300

    Added unit tests for InMemoryStatusService

---
+++ api/v1/status_test.go
@@ -0,0 +1,76 @@
+package v1
+
+import (
+	"testing"
+)
+
+
+func TestStatusNameString(t *testing.T) {
+	name := STATUS_SUCCEEDED
+	if name.String() != "SUCCEEDED" {
+		t.Fatal()
+	}
+}
+
+func TestInMemoryStatusServiceCreateGet(t *testing.T) {
+	service := NewInMemoryStatusService()
+	status := service.Create()
+
+	if len(status.Id) != 36 {
+		t.Fatal()
+	}
+
+	if status.Status != STATUS_PENDING {
+		t.Fatal()
+	}
+
+	statusId := status.Id
+	status, err := service.Get(statusId)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	if status.Id != statusId {
+		t.Fatal()
+	}
+
+	if status.Status != STATUS_PENDING {
+		t.Fatal()
+	}
+}
+
+func TestInMemoryStatusServiceGetFailure(t *testing.T) {
+	service := NewInMemoryStatusService()
+
+	status, err := service.Get("unknown id")
+
+	if status != nil {
+		t.Fatal()
+	}
+
+	if err == nil {
+		t.Fatal()
+	}
+
+	if err.Error() != "Status unknown id was not found" {
+		t.Fatal()
+	}
+}
+
+func TestInMemoryStatusServiceDelete(t *testing.T) {
+	service := NewInMemoryStatusService()
+	status := service.Create()
+
+	_, err := service.Get(status.Id)
+	if err != nil {
+		t.Fatal()
+	}
+
+	service.Delete(status.Id)
+
+	_, err = service.Get(status.Id)
+	if err == nil {
+		t.Fatal()
+	}
+}
