af914f08543b8350a8032523ba1c3c9b3b79bfb1
Aleksandr Danshyn <dalazx@gmail.com>
2018-04-26 17:02:31 +0300

    Added deployment automation

---
+++ deploy.mk
@@ -0,0 +1,26 @@
+ANSIBLE_DOCKER_ENV_PATH ?= /tmp/ansible.docker.env
+ANSIBLE_VAULT_PASSWORD_PATH ?= /tmp/ansible.vault
+
+.vault_pass:
+ifdef ANSIBLE_VAULT_PASSWORD
+	@echo $(ANSIBLE_VAULT_PASSWORD) > $(ANSIBLE_VAULT_PASSWORD_PATH)
+else
+	@cat ~/.vault_pass > $(ANSIBLE_VAULT_PASSWORD_PATH)
+endif
+
+ansible.docker.env:
+	@env | grep AWS_ > $(ANSIBLE_DOCKER_ENV_PATH)
+
+# in order to get SSH-forwarding working on macOS, follow the
+# instructions here:
+# https://github.com/uber-common/docker-ssh-agent-forward
+ANSIBLE_DOCKER_OPTS := \
+    -v ssh-agent:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent/ssh-agent.sock \
+    --env-file $(ANSIBLE_DOCKER_ENV_PATH) \
+    -v $(ANSIBLE_VAULT_PASSWORD_PATH):/root/.vault_pass \
+    registry.neuromation.io/neuromationorg/ansible:latest
+
+deploy_platformapi_dev: _docker_login ansible.docker.env .vault_pass
+	# TODO: pass tag
+	docker run --rm $(ANSIBLE_DOCKER_OPTS) \
+	    ansible-playbook -l 'tag_env_dev:&tag_role_master' platformapi_deploy.yml
