537414b82dde5b2d9728c67c77236acd59c444c7
Aleksandr Danshyn <dalazx@gmail.com>
2018-05-08 19:25:11 +0300

    Forcing pulling the ansible docker image

--- deploy.mk
+++ deploy.mk
@@ -11,11 +11,12 @@ endif
 ansible.docker.env:
 	@env | grep AWS_ > $(ANSIBLE_DOCKER_ENV_PATH)
 
+ANSIBLE_DOCKER_IMAGE := registry.neuromation.io/neuromationorg/ansible:latest
 ANSIBLE_DOCKER_OPTS := \
     -v ssh-agent:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent/ssh-agent.sock \
     --env-file $(ANSIBLE_DOCKER_ENV_PATH) \
     -v $(ANSIBLE_VAULT_PASSWORD_PATH):/root/.vault_pass \
-    registry.neuromation.io/neuromationorg/ansible:latest
+    $(ANSIBLE_DOCKER_IMAGE)
 
 _run_docker_ssh_agent_forward:
 	git clone git://github.com/uber-common/docker-ssh-agent-forward
@@ -23,5 +24,7 @@ _run_docker_ssh_agent_forward:
 
 deploy_platformapi_dev: _run_docker_ssh_agent_forward _docker_login ansible.docker.env .vault_pass
 	# TODO: pass tag
+	# forcing to use the latest available docker image
+	docker pull $(ANSIBLE_DOCKER_IMAGE)
 	docker run --rm $(ANSIBLE_DOCKER_OPTS) \
 	    ansible-playbook -l 'tag_env_dev:&tag_role_master' platformapi_deploy.yml
