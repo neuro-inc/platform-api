3949170a2b6df2390385824e930cc909cc1fb809
kyryl <truskovskiyk@gmail.com>
2018-05-17 11:35:38 +0300

    done with Job

--- platform_api/job.py
+++ platform_api/job.py
@@ -1,25 +1,29 @@
 from dataclasses import dataclass
+from orchestrator.orchestrator import Orchestrator
 
 
 @dataclass(frozen=True)
 class JobRequest:
-    job_name: str
+    job_id: str
     docker_image: str
     args: [str]
 
 
 class Job:
+    def __init__(self, orchestrator: Orchestrator, job_request: JobRequest):
+        self._orchestrator = orchestrator
+        self._job_request = job_request
+
     async def start(self):
-        pass
+        return await self._orchestrator.start_job(self._job_request)
 
     async def stop(self):
-        pass
-
-    async def delete(self):
-        pass
+        job_id = await self.get_id()
+        return await self._orchestrator.stop_job(job_id=job_id)
 
     async def status(self):
-        pass
+        job_id = await self.get_id()
+        return await self._orchestrator.job_status(job_id=job_id)
 
     async def get_id(self):
-        pass
+        return self._job_request.job_id
