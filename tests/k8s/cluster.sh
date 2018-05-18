63e09fc03255265850c7160a580c8b26f2fe3e01
Aleksandr Danshyn <dalazx@gmail.com>
2018-05-18 12:25:55 +0300

    Fixed the minikube version

--- tests/k8s/cluster.sh
+++ tests/k8s/cluster.sh
@@ -1,28 +1,38 @@
 #!/usr/bin/env bash
 
-function k8s::install {
-    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-    chmod +x minikube
-
+function k8s::install_kubectl {
     local kubectl_version=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
     curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/linux/amd64/kubectl
     chmod +x kubectl
+    sudo mv kubectl /usr/local/bin/
+}
+function k8s::install_minikube {
+    local minikube_version=v0.25.2
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/minikube_version/minikube-linux-amd64
+    chmod +x minikube
+    sudo mv minikube /usr/local/bin/
+}
+
+function k8s::install {
+    k8s::install_kubectl
+    k8s::install_minikube
 }
 
 function k8s::start {
     export KUBECONFIG=$HOME/.kube/config
+    mkdir -p $(dirname $KUBECONFIG)
+    touch $KUBECONFIG
+
     export MINIKUBE_WANTUPDATENOTIFICATION=false
     export MINIKUBE_WANTREPORTERRORPROMPT=false
     export MINIKUBE_HOME=$HOME
     export CHANGE_MINIKUBE_NONE_USER=true
-    mkdir -p $(dirname $KUBECONFIG)
-    touch $KUBECONFIG
 
-    sudo -E ./minikube start --vm-driver=none
+    sudo -E minikube start --vm-driver=none
 
     # this for loop waits until kubectl can access the api server that Minikube has created
     for i in {1..150}; do # timeout for 5 minutes
-        ./kubectl get po &> /dev/null
+        kubectl get po &> /dev/null
         if [ $? -ne 1 ]; then
             break
         fi
@@ -31,7 +41,7 @@ function k8s::start {
 }
 
 function k8s::stop {
-    sudo -E ./minikube stop
+    sudo -E minikube stop
 }
 
 
