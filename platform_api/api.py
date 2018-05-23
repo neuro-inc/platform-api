7b3296e8d771651a704fb63befd672ad9ac3fd87
kyryl <truskovskiyk@gmail.com>
2018-05-23 16:31:41 +0300

    add create_kube_orchestrator

--- platform_api/api.py
+++ platform_api/api.py
@@ -1,11 +1,13 @@
 import asyncio
 import logging
+import json
 
 import aiohttp.web
 
 
 from .config import Config
 from .models import ModelsHandler
+from .orchestrator import KubeOrchestrator, KubeConfig
 
 
 class ApiHandler:
@@ -41,6 +43,45 @@ async def handle_exceptions(request, handler):
         return aiohttp.web.json_response(
             payload, status=aiohttp.web.HTTPInternalServerError.status_code)
 
+async def create_kube_orchestrator(loop: asyncio.AbstractEventLoop)
+    # TODO remove it
+    process = await asyncio.create_subprocess_exec(
+        'kubectl', 'config', 'view', '-o', 'json',
+        stdout=asyncio.subprocess.PIPE)
+    output, _ = await process.communicate()
+    payload_str = output.decode().rstrip()
+    kube_config_payload = json.loads(payload_str)
+
+    cluster_name = 'minikube'
+    clusters = {
+        cluster['name']: cluster['cluster']
+        for cluster in kube_config_payload['clusters']}
+    cluster = clusters[cluster_name]
+
+    user_name = 'minikube'
+    users = {
+        user['name']: user['user']
+        for user in kube_config_payload['users']}
+    user = users[user_name]
+
+    kube_config = KubeConfig(
+        endpoint_url=cluster['server'],
+        cert_authority_path=cluster['certificate-authority'],
+        auth_cert_path=user['client-certificate'],
+        auth_cert_key_path=user['client-key']
+    )
+
+    kube_orchestrator = KubeOrchestrator(config=kube_config, loop=loop)
+    return kube_orchestrator
+
+
+async def create_models_app(config: Config):
+    models_app = aiohttp.web.Application()
+    kube_orchestrator = create_kube_orchestrator(models_app.loop)
+    models_handler = ModelsHandler(orchestrator=kube_orchestrator)
+    models_handler.register(models_app)
+    return models_app
+
 
 async def create_app(config: Config):
     app = aiohttp.web.Application(middlewares=[handle_exceptions])
@@ -50,9 +91,7 @@ async def create_app(config: Config):
     api_v1_handler = ApiHandler()
     api_v1_handler.register(api_v1_app)
 
-    models_app = aiohttp.web.Application()
-    models_handler = ModelsHandler()
-    models_handler.register(models_app)
+    models_app = create_models_app(config)
     api_v1_app.add_subapp('/models', models_app)
 
     app.add_subapp('/api/v1', api_v1_app)
