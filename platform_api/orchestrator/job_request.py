51b3dd9ed1dda3e33e77253261a99ab69eac9bfb
Aleksandr Danshyn <dalazx@gmail.com>
2018-05-23 15:51:16 +0300

    Added Container and ContainerVolume

--- platform_api/orchestrator/job_request.py
+++ platform_api/orchestrator/job_request.py
@@ -1,6 +1,6 @@
 import enum
-from dataclasses import dataclass
-from typing import Optional, List
+from dataclasses import dataclass, field
+from typing import Dict, Optional, List
 
 
 class JobError(Exception):
@@ -8,11 +8,25 @@ class JobError(Exception):
 
 
 @dataclass(frozen=True)
+class ContainerVolume:
+    src_path: str
+    dst_path: str
+    read_only: bool = False
+
+
+@dataclass(frozen=True)
+class Container:
+    image: str
+    # TODO (A Danshyn 05/23/18): command and env are not integrated yet
+    command: Optional[str] = None
+    env: Dict[str, str] = field(default_factory=dict)
+    volumes: List[ContainerVolume] = field(default_factory=list)
+
+
+@dataclass(frozen=True)
 class JobRequest:
     job_id: str
-    container_name: str
-    docker_image: str
-    args: Optional[List[str]] = None
+    container: Container
 
 
 class JobStatus(str, enum.Enum):
