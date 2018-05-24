d495d097608a5e66e42e9a446d59b716b718c4e0
Aleksandr Danshyn <dalazx@gmail.com>
2018-05-24 18:19:34 +0300

    Some basic tests

--- tests/unit/test_job.py
+++ tests/unit/test_job.py
@@ -1,4 +1,9 @@
-from platform_api.orchestrator.job_request import Container
+from pathlib import PurePath
+
+import pytest
+
+from platform_api.orchestrator.job_request import (
+    Container, ContainerVolume, ContainerVolumeFactory)
 
 
 class TestContainer:
@@ -9,3 +14,30 @@ class TestContainer:
     def test_command_list(self):
         container = Container(image='testimage', command='bash -c date')
         assert container.command_list == ['bash', '-c', 'date']
+
+
+class TestContainerVolumeFactory:
+    def test_invalid_storage_uri_scheme(self):
+        uri = 'invalid://path'
+        with pytest.raises(ValueError, match='Invalid scheme'):
+            ContainerVolumeFactory(
+                uri, src_mount_path=PurePath('/'),
+                dst_mount_path=PurePath('/'))
+
+    def test_invalid_storage_uri_path(self):
+        uri = 'storage://'
+        with pytest.raises(ValueError, match='Empty path'):
+            ContainerVolumeFactory(
+                uri, src_mount_path=PurePath('/'),
+                dst_mount_path=PurePath('/'))
+
+    def test_create(self):
+        uri = 'storage://path/to/dir'
+        volume = ContainerVolumeFactory(
+            uri,
+            src_mount_path=PurePath('/host'),
+            dst_mount_path=PurePath('/container')
+        ).create()
+        assert volume.src_path == '/host/path/to/dir'
+        assert volume.dst_path == '/container/path/to/dir'
+        assert not volume.read_only
