f91f550e9b5fbb668f392ffc78d6a9c91b26c607
Aleksandr Danshyn <dalazx@gmail.com>
2018-06-25 18:04:47 +0300

    Extended Job

--- platform_api/orchestrator/job.py
+++ platform_api/orchestrator/job.py
@@ -1,26 +1,38 @@
-from .job_request import JobRequest, JobStatus
+from typing import Dict
+
+from .job_request import Container, JobRequest, JobStatus
 from .base import Orchestrator
 
 
 class Job:
     def __init__(
-            self, orchestrator: Orchestrator, job_request: JobRequest
-            ) -> None:
+            self, orchestrator: Orchestrator, job_request: JobRequest,
+            status: JobStatus=JobStatus.PENDING) -> None:
+        # TODO: replace Orchestrator with OrchestratorConfig/KubeConfig
         self._orchestrator = orchestrator
         self._job_request = job_request
-
-    async def start(self) -> JobStatus:
-        return await self._orchestrator.start_job(self._job_request)
-
-    async def delete(self) -> JobStatus:
-        return await self._orchestrator.delete_job(job_id=self.id)
-
-    async def status(self) -> JobStatus:
-        return await self._orchestrator.status_job(job_id=self.id)
+        # TODO: introduce JobStatus object with diagnostic info
+        self._status = status
 
     @property
     def id(self):
         return self._job_request.job_id
+
+    @property
+    def request(self) -> JobRequest:
+        return self._job_request
+
+    @property
+    def status(self) -> JobStatus:
+        return self._status
+
+    @status.setter
+    def status(self, value: JobStatus) -> None:
+        self._status = value
+
+    @property
+    def is_finished(self) -> bool:
+        return self._status.is_finished
 
     @property
     def has_http_server_exposed(self) -> bool:
@@ -31,3 +43,17 @@ class Job:
         assert self.has_http_server_exposed
         jobs_domain_name = self._orchestrator.config.jobs_ingress_domain_name
         return f'http://{self.id}.{jobs_domain_name}'
+
+    def to_primitive(self) -> Dict:
+        return {
+            'id': self.id,
+            'request': self.request.to_primitive(),
+            'status': self._status.value,
+        }
+
+    @classmethod
+    def from_primitive(cls, orchestrator: Orchestrator, payload: Dict) -> 'Job':
+        job_request = JobRequest.from_primitive(payload['request'])
+        status = JobStatus(payload['status'])
+        return cls(
+            orchestrator=orchestrator, job_request=job_request, status=status)
