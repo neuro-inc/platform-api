bd4cd707cb4b71d4e10a736d211338e7406441ea
Aleksandr Danshyn <dalazx@gmail.com>
2018-07-02 13:27:53 +0300

    Extra cleanup

--- tests/k8s/cluster.sh
+++ tests/k8s/cluster.sh
@@ -34,6 +34,7 @@ function k8s::start {
     export MINIKUBE_HOME=$HOME
     export CHANGE_MINIKUBE_NONE_USER=true
 
+    sudo -E minikube config set WantReportErrorPrompt false
     sudo -E minikube start --vm-driver=none
 
     k8s::wait
