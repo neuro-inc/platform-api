336195d32808f35473304c3df273895b17ac78c1
Aleksandr Danshyn <dalazx@gmail.com>
2018-07-05 15:19:19 +0300

    Added a new handler for job log

--- platform_api/handlers/jobs_handler.py
+++ platform_api/handlers/jobs_handler.py
@@ -12,6 +12,7 @@ class JobsHandler:
             aiohttp.web.get('', self.handle_get_all),
             aiohttp.web.delete('/{job_id}', self.handle_delete),
             aiohttp.web.get('/{job_id}', self.handle_get),
+            aiohttp.web.get('/{job_id}/log', self.stream_log),
         ))
 
     async def handle_get(self, request):
@@ -30,3 +31,21 @@ class JobsHandler:
         job_id = request.match_info['job_id']
         await self._jobs_service.delete_job(job_id)
         return aiohttp.web.HTTPNoContent()
+
+    async def stream_log(self, request):
+        job_id = request.match_info['job_id']
+        log_reader = await self._jobs_service.get_job_log_reader(job_id)
+        chunk_size = 1024
+
+        response = aiohttp.web.StreamResponse(status=200)
+        await response.prepare(request)
+
+        async with log_reader:
+            while True:
+                chunk = await log_reader.read(size=chunk_size)
+                if not chunk:
+                    break
+                await response.write(chunk)
+
+        await response.write_eof()
+        return response
