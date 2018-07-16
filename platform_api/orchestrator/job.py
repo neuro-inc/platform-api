6d5ea308490e19f4808dde35a784381d96dfecb8
Aleksandr Danshyn <dalazx@gmail.com>
2018-07-16 19:08:04 +0300

    Added Job.should_be_deleted

--- platform_api/orchestrator/job.py
+++ platform_api/orchestrator/job.py
@@ -1,18 +1,28 @@
-from typing import Dict
+from datetime import datetime, timedelta, timezone
+from functools import partial
+from typing import Dict, Optional
 
 from ..config import OrchestratorConfig  # noqa
 from .job_request import JobRequest, JobStatus
+
+current_datetime_factory = partial(datetime.now, timezone.utc)
 
 
 class Job:
     def __init__(
             self, orchestrator_config: OrchestratorConfig,
             job_request: JobRequest,
-            status: JobStatus=JobStatus.PENDING) -> None:
+            status: JobStatus=JobStatus.PENDING,
+            current_datetime_factory=current_datetime_factory) -> None:
         self._orchestrator_config = orchestrator_config
         self._job_request = job_request
         # TODO: introduce JobStatus object with diagnostic info
         self._status = status
+
+        self._finished_at: Optional[datetime] = None
+        self._is_deleted: bool = False
+
+        self._current_datetime_factory = current_datetime_factory
 
     @property
     def id(self):
@@ -29,10 +39,42 @@ class Job:
     @status.setter
     def status(self, value: JobStatus) -> None:
         self._status = value
+        if self.is_finished and not self._finished_at:
+            self._finished_at = self._current_datetime_factory()
 
     @property
     def is_finished(self) -> bool:
         return self._status.is_finished
+
+    @property
+    def finished_at(self) -> Optional[datetime]:
+        return self._finished_at
+
+    @property
+    def is_deleted(self) -> bool:
+        return self._is_deleted
+
+    @is_deleted.setter
+    def is_deleted(self, value: bool) -> None:
+        self._is_deleted = value
+
+    @property
+    def _deletion_planned_at(self) -> Optional[datetime]:
+        if not self.finished_at:
+            return None
+
+        return (
+            self.finished_at + self._orchestrator_config.job_deletion_delay)
+
+    @property
+    def _is_time_for_deletion(self) -> bool:
+        return self._deletion_planned_at <= self._current_datetime_factory()
+
+    @property
+    def should_be_deleted(self) -> bool:
+        return (
+            self.is_finished and not self._is_deleted and
+            self._is_time_for_deletion)
 
     @property
     def has_http_server_exposed(self) -> bool:
