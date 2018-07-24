48a537ffa7760a6613e0f2ffd1aa858941ed697e
Aleksandr Danshyn <dalazx@gmail.com>
2018-07-24 15:15:18 -0400

    Added docker fixtures

---
+++ tests/integration/docker.py
@@ -0,0 +1,23 @@
+import aiodocker
+import pytest
+
+
+PYTEST_REUSE_DOCKER_OPT = '--reuse-docker'
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        PYTEST_REUSE_DOCKER_OPT, action='store_true',
+        help='Reuse existing docker containers')
+
+
+@pytest.fixture(scope='session')
+def reuse_docker(request):
+    return request.config.getoption(PYTEST_REUSE_DOCKER_OPT)
+
+
+@pytest.fixture(scope='session')
+async def docker():
+    client = aiodocker.Docker()
+    yield client
+    await client.close()
