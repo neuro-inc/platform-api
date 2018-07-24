ef9ceb5ee3b93620d252d78659a95584d35ddcd9
Aleksandr Danshyn <dalazx@gmail.com>
2018-07-24 18:41:20 -0400

    Added RedisJobsStorage

--- platform_api/orchestrator/jobs_storage.py
+++ platform_api/orchestrator/jobs_storage.py
@@ -1,10 +1,12 @@
 import json
 from abc import ABC, abstractmethod
-from typing import Dict, List
+from typing import Any, Dict, List
+
+import aioredis
 
 from .base import Orchestrator
 from .job import Job
-from .job_request import JobError
+from .job_request import JobError, JobStatus
 
 
 class JobsStorage(ABC):
@@ -60,3 +62,69 @@ class InMemoryJobsStorage(JobsStorage):
         for payload in self._job_records.values():
             jobs.append(self._parse_job_payload(payload))
         return jobs
+
+
+class RedisJobsStorage(JobsStorage):
+    def __init__(
+            self, client: aioredis.Redis, orchestrator: Orchestrator) -> None:
+        self._client = client
+        self._orchestrator = orchestrator
+
+    def _generate_job_key(self, job_id: str) -> str:
+        return f'jobs:{job_id}'
+
+    def _generate_jobs_status_index_key(self, status: JobStatus) -> str:
+        return f'jobs.status.{status}'
+
+    def _generate_jobs_index_key(self) -> str:
+        return 'jobs'
+
+    async def set_job(self, job: Job) -> None:
+        payload = json.dumps(job.to_primitive())
+
+        tr = self._client.multi_exec()
+        tr.set(self._generate_job_key(job.id), payload)
+        tr.zadd('jobs', 0, job.id)
+        for status in JobStatus:
+            tr.zrem(self._generate_jobs_status_index_key(status), job.id)
+        tr.zadd(self._generate_jobs_status_index_key(job.status), 0, job.id)
+        await tr.execute()
+
+    def _parse_job_payload(self, payload: str) -> Job:
+        job_record = json.loads(payload)
+        return Job.from_primitive(self._orchestrator.config, job_record)
+
+    async def get_job(self, job_id: str) -> Job:
+        payload = await self._client.get(self._generate_job_key(job_id))
+        if payload is None:
+            raise JobError(f'no such job {job_id}')
+        return self._parse_job_payload(payload)
+
+    async def _get_jobs(self, ids: List[str]) -> List[Job]:
+        keys = [self._generate_job_key(id_) for id_ in ids]
+        jobs = []
+        for payload in await self._client.mget(*keys):
+            jobs.append(self._parse_job_payload(payload))
+        return jobs
+
+    async def _get_all_job_ids(self) -> List[str]:
+        job_ids = []
+        async for job_id, _ in self._client.izscan(
+                self._generate_jobs_index_key()):
+            job_ids.append(job_id.decode())
+        return job_ids
+
+    async def _get_running_job_ids(self) -> List[str]:
+        job_ids = []
+        async for job_id, _ in self._client.izscan(
+                self._generate_jobs_status_index_key(JobStatus.RUNNING)):
+            job_ids.append(job_id.decode())
+        return job_ids
+
+    async def get_all_jobs(self) -> List[Job]:
+        job_ids = await self._get_all_job_ids()
+        return await self._get_jobs(job_ids)
+
+    async def get_running_jobs(self) -> List[Job]:
+        job_ids = await self._get_running_job_ids()
+        return await self._get_jobs(job_ids)
