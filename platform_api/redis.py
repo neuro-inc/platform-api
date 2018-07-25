4c94b6754fff63fa3d2fc42426166fb1fce8adb5
Aleksandr Danshyn <dalazx@gmail.com>
2018-07-25 10:14:14 -0400

    Added RedisConfig and a factory

---
+++ platform_api/redis.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+import aioredis
+from async_generator import asynccontextmanager
+
+
+@dataclass(frozen=True)
+class RedisConfig:
+    uri: str
+
+    conn_pool_size: int = 10
+    conn_timeout_s: float = 10
+
+
+@asynccontextmanager
+async def create_redis_client(config: RedisConfig) -> aioredis.Redis:
+    client = await aioredis.create_redis_pool(
+        config.uri, maxsize=config.conn_pool_size,
+        timeout=config.conn_timeout_s)
+    try:
+        yield client
+    finally:
+        client.close()
+        await client.wait_closed()
