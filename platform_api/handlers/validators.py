937d50ccb78fe5492a8ca60660f4be57216ae59f
Aleksandr Danshyn <dalazx@gmail.com>
2018-07-30 13:02:23 -0400

    Added create_volume_request_validator

--- platform_api/handlers/validators.py
+++ platform_api/handlers/validators.py
@@ -1,8 +1,22 @@
 import trafaret as t
 
 
-def create_container_request_validator() -> t.Trafaret:
+def create_volume_request_validator() -> t.Trafaret:
     return t.Dict({
+        'src_storage_uri': t.String,
+        'dst_path': t.String,
+        t.Key('read_only', optional=True): t.Bool(default=False),
+    })
+
+
+def create_container_request_validator(
+        allow_volumes: bool = False) -> t.Trafaret:
+    """Create a validator for primitive container objects.
+
+    Meant to be used in high-level resources such as jobs, models, batch
+    inference etc.
+    """
+    validator = t.Dict({
         'image': t.String,
         t.Key('command', optional=True): t.String,
         t.Key('env', optional=True): t.Mapping(
@@ -17,3 +31,11 @@ def create_container_request_validator() -> t.Trafaret:
             t.Key('health_check_path', optional=True): t.String,
         }),
     })
+
+    if allow_volumes:
+        volume_validator = create_volume_request_validator()
+        validator += t.Dict({
+            t.Key('volumes', optional=True): t.List(volume_validator),
+        })
+
+    return validator
