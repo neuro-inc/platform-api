2884825517207ceaf0bdbf2c881558d9494c57f8
Aleksandr Danshyn <dalazx@gmail.com>
2018-09-10 14:10:04 +0300

    Updated an integration tests that excepts JobError

--- tests/integration/test_kube_orchestrator.py
+++ tests/integration/test_kube_orchestrator.py
@@ -8,7 +8,8 @@ import pytest
 from async_timeout import timeout
 
 from platform_api.orchestrator import (
-    Job, JobError, JobRequest, JobStatus, LogReader, Orchestrator
+    Job, JobError, JobNotFoundException, JobRequest, JobStatus, LogReader,
+    Orchestrator
 )
 from platform_api.orchestrator.job import JobStatusItem
 from platform_api.orchestrator.job_request import (
@@ -394,7 +395,7 @@ async def delete_pod_later(kube_client):
 class TestKubeClient:
     @pytest.mark.asyncio
     async def test_wait_pod_is_running_not_found(self, kube_client):
-        with pytest.raises(JobError):
+        with pytest.raises(JobNotFoundException):
             await kube_client.wait_pod_is_running(pod_name='unknown')
 
     @pytest.mark.asyncio
