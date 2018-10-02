6915cfad1eea2abb3f50ce2ce97a55edc63680f9
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-10-02 17:17:18 +0300

    Combine coverage reports

--- k8s.mk
+++ k8s.mk
@@ -28,7 +28,9 @@ clean_k8s: stop_k8s
 test_k8s_platform_api:
 	kubectl config view
 	make test_unit
+	mv .coverage .coverage.unit
 	make test_integration
+	coverage combine
 
 test_k8s_platform_api_e2e: build_api_k8s
 	./run_e2e_tests.sh
