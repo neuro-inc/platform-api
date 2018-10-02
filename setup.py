5216782241c2e6506df531359add1c41421593a4
Aleksandr Danshyn <dalazx@gmail.com>
2018-10-02 21:51:12 +0300

    Bumped dataclasses

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = (
     "aioredis==1.1.0",
     "async-exit-stack==1.0.1",  # backport from 3.7 stdlib
     "async-generator==1.9",
-    "dataclasses==0.5",  # backport from 3.7 stdlib
+    "dataclasses==0.6",  # backport from 3.7 stdlib
     "iso8601==0.1.12",
     "trafaret==1.1.1",
     "neuro_auth_client==0.0.1b3",
