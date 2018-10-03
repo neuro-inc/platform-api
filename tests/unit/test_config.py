4e2bc1a439b9bd3657836a04115ee52a37721db7
Aleksandr Danshyn <dalazx@gmail.com>
2018-10-03 14:15:02 +0300

    Forcing failure

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -14,6 +14,7 @@ from platform_api.orchestrator.kube_orchestrator import (
 
 class TestStorageConfig:
     def test_missing_nfs_settings(self):
+        assert False
         with pytest.raises(ValueError, match="Missing NFS settings"):
             StorageConfig(host_mount_path=PurePath("/tmp"), type=StorageType.NFS)
 
