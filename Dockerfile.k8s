6ff82a1b7a649ca0b59e9a1fe55fd061329dd7cc
Aleksandr Danshyn <dalazx@gmail.com>
2018-10-03 10:56:25 +0300

    Auth client dependency (#202)
    * Passed PIP_INDEX_URL
    
    * Added neuro_auth_client
    
    * Fixed formatting issues
    
    * Bumped dataclasses
    
    * Bumped aiohttp

--- Dockerfile.k8s
+++ Dockerfile.k8s
@@ -2,6 +2,8 @@ FROM python:3.6.5-stretch
 
 WORKDIR /neuromation
 
+ARG PIP_INDEX_URL
+
 # installing dependencies ONLY
 COPY setup.py ./
 RUN \
--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import find_packages, setup
 
 
 install_requires = (
-    "aiohttp==3.4.3",
+    "aiohttp==3.4.4",
     # WARN: aioredis does not support Redis Cluster yet
     "aioredis==1.1.0",
     "async-exit-stack==1.0.1",  # backport from 3.7 stdlib
     "async-generator==1.9",
-    "dataclasses==0.5",  # backport from 3.7 stdlib
+    "dataclasses==0.6",  # backport from 3.7 stdlib
     "iso8601==0.1.12",
     "trafaret==1.1.1",
+    "neuro_auth_client==0.0.1b3",
 )
 
 setup(
