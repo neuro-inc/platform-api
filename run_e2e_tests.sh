2981841d9cb8748497b0579dc662e3ef0c7b8210
Aleksandr Danshyn <dalazx@gmail.com>
2018-10-04 15:39:51 +0300

    Put a note on running e2e locally agains minikube

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -2,7 +2,15 @@
 
 docker tag $(cat AUTH_SERVER_IMAGE_NAME) platformauthapi:latest
 
-kubectl create -f deploy/rb.default.gke.yml
+# NOTE (A Danshyn 10/04/18): in order to run this test locally using a VM-based
+# minikube, one needs to uncomment these lines first
+# docker save -o /tmp/platformauthapi.image platformauthapi:latest
+# docker save -o /tmp/platformapi.image platformapi-k8s:latest
+# eval $(minikube docker-env)
+# docker load -i /tmp/platformauthapi.image
+# docker load -i /tmp/platformapi.image
+
+kubectl create -f deploy/platformapi/templates/rb.default.gke.yml
 kubectl create -f tests/k8s/platformapi.yml
 
 attempt=1
