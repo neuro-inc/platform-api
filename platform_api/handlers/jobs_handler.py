b61a95332116db790f56138b5f3c846e3d7c9e8f
Aleksandr Danshyn <dalazx@gmail.com>
2018-10-09 18:49:00 +0300

    Checking storage URIs on job submission

--- platform_api/handlers/jobs_handler.py
+++ platform_api/handlers/jobs_handler.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import PurePath
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 import aiohttp.web
 import trafaret as t
@@ -11,8 +11,8 @@ from yarl import URL
 from platform_api.config import Config, StorageConfig
 from platform_api.orchestrator import JobsService
 from platform_api.orchestrator.job import Job
-from platform_api.orchestrator.job_request import ContainerVolume, JobRequest
-from platform_api.user import untrusted_user
+from platform_api.orchestrator.job_request import Container, ContainerVolume, JobRequest
+from platform_api.user import User, untrusted_user
 
 from .job_request_builder import ContainerBuilder
 from .validators import (
@@ -123,6 +123,17 @@ def convert_job_to_job_response(
     return response_payload
 
 
+def infer_permissions_from_container(
+    user: User, container: Container
+) -> List[Permission]:
+    permissions = [Permission(uri=str(user.to_job_uri()), action="write")]
+    for volume in container.volumes:
+        action = "read" if volume.read_only else "write"
+        permission = Permission(uri=str(volume.uri), action=action)
+        permissions.append(permission)
+    return permissions
+
+
 class JobsHandler:
     def __init__(self, *, app: aiohttp.web.Application, config: Config) -> None:
         self._app = app
@@ -152,15 +163,17 @@ class JobsHandler:
 
     async def create_job(self, request):
         user = await untrusted_user(request)
-        permission = Permission(uri=str(user.to_job_uri()), action="write")
-        logger.info("Checking whether %r has %r", user, permission)
-        await check_permission(request, permission.action, [permission])
 
         orig_payload = await request.json()
         request_payload = self._job_request_validator.check(orig_payload)
         container = ContainerBuilder.from_container_payload(
             request_payload["container"], storage_config=self._storage_config
         ).build()
+
+        permissions = infer_permissions_from_container(user, container)
+        logger.info("Checking whether %r has %r", user, permissions)
+        await check_permission(request, next(iter(permissions)).action, permissions)
+
         job_request = JobRequest.create(container)
         job, _ = await self._jobs_service.create_job(job_request, user=user)
         response_payload = convert_job_to_job_response(job, self._storage_config)
