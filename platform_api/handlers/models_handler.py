15794494d3d2c92f7cd856cb1b4470400f941ffc
Aleksandr Danshyn <dalazx@gmail.com>
2018-10-09 18:49:31 +0300

    Checking storage URIs on models submissions

--- platform_api/handlers/models_handler.py
+++ platform_api/handlers/models_handler.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Dict
+from typing import Any, Dict
 
 import aiohttp.web
 import trafaret as t
@@ -8,9 +8,11 @@ from neuro_auth_client import Permission
 
 from platform_api.config import Config
 from platform_api.orchestrator import JobRequest, JobsService
+from platform_api.orchestrator.job_request import Container
 from platform_api.user import User, untrusted_user
 
 from .job_request_builder import ModelRequest
+from .jobs_handler import infer_permissions_from_container
 from .validators import create_container_request_validator, create_job_status_validator
 
 
@@ -60,8 +62,7 @@ class ModelsHandler:
             )
         )
 
-    async def _create_job(self, model_request: ModelRequest, user: User) -> Dict:
-        container = model_request.to_container()
+    async def _create_job(self, user: User, container: Container) -> Dict[str, Any]:
         job_request = JobRequest.create(container)
         job, status = await self._jobs_service.create_job(job_request, user=user)
         payload = {"job_id": job.id, "status": status.value}
@@ -71,20 +72,21 @@ class ModelsHandler:
 
     async def handle_post(self, request):
         user = await untrusted_user(request)
-        permission = Permission(uri=str(user.to_job_uri()), action="write")
-        logger.info("Checking whether %r has %r", user, permission)
-        await check_permission(request, permission.action, [permission])
 
         orig_payload = await request.json()
         request_payload = self._model_request_validator.check(orig_payload)
 
-        model_request = ModelRequest(
+        container = ModelRequest(
             request_payload,
             storage_config=self._storage_config,
             env_prefix=self._config.env_prefix,
-        )
+        ).to_container()
 
-        response_payload = await self._create_job(model_request, user=user)
+        permissions = infer_permissions_from_container(user, container)
+        logger.info("Checking whether %r has %r", user, permissions)
+        await check_permission(request, next(iter(permissions)).action, permissions)
+
+        response_payload = await self._create_job(user, container)
         self._model_response_validator.check(response_payload)
 
         return aiohttp.web.json_response(
