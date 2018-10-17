d77676814a361939043d60831bf7df038361575c
alin999 <alinduica@gmail.com>
2018-10-17 20:51:11 +0300

    jumphost-persistent-disk for /home + code to re-add users

--- deploy/jumphost/docker/entrypoint.sh
+++ deploy/jumphost/docker/entrypoint.sh
@@ -9,4 +9,6 @@ touch /root/.ssh/authorized_keys
 echo ${NP_JUMP_HOST_ROOT_TOKEN} > /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 
+find /home/*/.ssh -type d |while read -r line ; do arr=(${line//// }); adduser -d /home/${arr[1]} ${arr[1]}; done
+
 /usr/sbin/sshd -D
