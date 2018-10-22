33857c4e636aea521f9618eaceac19ad6c127e74
Rafael Zubairov <zubra.bubra@gmail.com>
2018-10-22 15:37:05 +0300

    Restore users from github in case they do not exist.

--- deploy/jumphost/docker/entrypoint.sh
+++ deploy/jumphost/docker/entrypoint.sh
@@ -17,4 +17,61 @@ do
 	adduser -d /home/${name} ${name};
 	chown -R ${name}:${name} /home/${name};
 done
+
+#Add current team member set to bastion host
+users=(
+  "adavydow"
+  "aleksartamonov"
+  "alekseysh"
+  "alexander-rakhlin"
+  "alias2696"
+  "angusroven"
+  "ajuszkowski"
+  "astaff"
+  "asvetlov"
+  "bennylilom"
+  "berenigo"
+  "dalazx"
+  "dariaposrednikova"
+  "didcv"
+  "doctoraugust"
+  "fedor-s"
+  "fedorsavchenko"
+  "freestlr"
+  "genn"
+  "gigigheorghiceanu"
+  "golovanovsrg"
+  "kykyxdd"
+  "lastperson"
+  "mgdskr"
+  "nadia-wednesday"
+  "ne-bo"
+  "nebeskey"
+  "oanuthriveio"
+  "oktai15"
+  "pglolo"
+  "rauf-kurbanov"
+  "ryter"
+  "shagren"
+  "snikolenko"
+  "startapista"
+  "stasbel"
+  "sudodoki"
+  "truskovskiyk"
+  "vasiliy28"
+  "yagosu"
+  "ybehzadi"
+  "zubrabubra"
+)
+
+for user in ${users[@]}; do
+    echo $user
+    if [ ! -d "$DIRECTORY" ]; then
+        adduser -d /home/${name} ${name};
+        chown -R ${name}:${name} /home/${name};
+        curl https://github.com/${user}.keys >> /home/${user}/.ssh/authorized_keys
+        chmod 600 /home/${user}/.ssh/authorized_keys
+    fi
+done
+
 /usr/sbin/sshd -D
