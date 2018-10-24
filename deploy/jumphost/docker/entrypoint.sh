888c18aa14f5ead68671934018c02e989275d62b
Rafael Zubairov <zubrabubra@users.noreply.github.com>
2018-10-24 11:51:53 +0300

    User creation script - added missing but required parts: (#261)
    1. create .ssh directory in case it does not exist;
    2. chmod .ssh directory to 700;

--- deploy/jumphost/docker/entrypoint.sh
+++ deploy/jumphost/docker/entrypoint.sh
@@ -68,9 +68,11 @@ for user in ${users[@]}; do
     echo $user
     if [ ! -d "/home/${user}" ]; then
         adduser -d /home/${user} ${user};
-        chown -R ${user}:${user} /home/${user};
+        mkdir /home/${user}/.ssh;
         curl https://github.com/${user}.keys >> /home/${user}/.ssh/authorized_keys
+        chown -R ${user}:${user} /home/${user};
         chmod 600 /home/${user}/.ssh/authorized_keys
+        chmod 700 /home/${user}/.ssh
     fi
 done
 
