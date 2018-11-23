58dce1f62c2dc299a95a5d38673ae167a982ce99
Aleksandr Danshyn <dalazx@gmail.com>
2018-11-23 14:43:47 +0200

    More unit tests

--- tests/unit/test_kube_client.py
+++ tests/unit/test_kube_client.py
@@ -1,8 +1,11 @@
 import pytest
 
 from platform_api.orchestrator.kube_client import (
+    NodeAffinity,
+    NodePreferredSchedulingTerm,
     NodeSelectorOperator,
     NodeSelectorRequirement,
+    NodeSelectorTerm,
 )
 
 
@@ -22,17 +25,54 @@ class TestNodeSelectorRequirement:
         assert req == NodeSelectorRequirement(
             key="testkey", operator=NodeSelectorOperator.EXISTS
         )
-        assert req.to_primitive() == {
-            "key": "testkey",
-            "operator": "Exists",
-        }
+        assert req.to_primitive() == {"key": "testkey", "operator": "Exists"}
 
     def test_create_does_not_exist(self):
         req = NodeSelectorRequirement.create_does_not_exist("testkey")
         assert req == NodeSelectorRequirement(
             key="testkey", operator=NodeSelectorOperator.DOES_NOT_EXIST
         )
-        assert req.to_primitive() == {
-            "key": "testkey",
-            "operator": "DoesNotExist",
+        assert req.to_primitive() == {"key": "testkey", "operator": "DoesNotExist"}
+
+
+class TestNodeSelectorTerm:
+    def test_empty(self):
+        with pytest.raises(ValueError, match="no expressions"):
+            NodeSelectorTerm([])
+
+
+class TestNodeAffinity:
+    def test_empty(self):
+        with pytest.raises(ValueError, match="no terms"):
+            NodeAffinity()
+
+    def test_to_primitive(self):
+        node_affinity = NodeAffinity(
+            required=[
+                NodeSelectorTerm([NodeSelectorRequirement.create_exists("testkey")])
+            ],
+            preferred=[
+                NodePreferredSchedulingTerm(
+                    NodeSelectorTerm(
+                        [NodeSelectorRequirement.create_does_not_exist("anotherkey")]
+                    )
+                )
+            ],
+        )
+        assert node_affinity.to_primitive() == {
+            "requiredDuringSchedulingIgnoredDuringExecution": {
+                "nodeSelectorTerms": [
+                    {"matchExpressions": [{"key": "testkey", "operator": "Exists"}]}
+                ]
+            },
+            "preferredDuringSchedulingIgnoredDuringExecution": [
+                {
+                    "preference": {
+                        "matchExpressions": [
+                            {"key": "anotherkey", "operator": "DoesNotExist"}
+                        ]
+                    },
+                    "weight": 100,
+                }
+            ],
         }
