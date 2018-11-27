ea88b7fd8a3792db4d8a668bb59cf108902b24a3
Rafael Zubairov <zubra.bubra@gmail.com>
2018-11-27 11:56:13 +0300

    GCP OAuth2 integration component.

--- platform_api/config.py
+++ platform_api/config.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass, field
 from datetime import timedelta
 from enum import Enum
+from math import ceil
 from pathlib import PurePath
 from typing import Optional, Sequence
 
+import aiohttp
+from jose import constants, jwt
 from yarl import URL
 
 from .redis import RedisConfig
@@ -63,6 +66,84 @@ class StorageConfig:
     @classmethod
     def create_host(cls, *args, **kwargs) -> "StorageConfig":
         return cls(*args, **kwargs)  # type: ignore
+
+
+@dataclass(frozen=True)
+class GCPTokenStoreConfig:
+    # extracted private key part from service key file
+    private_key: str
+    # Service email address, to be provided from the conf
+    iss: str
+    # Expiration time of the short living token, in seconds, to be provided from conf
+    token_expiration: int = 600
+    # What URL would get our JWT token, and provide with short living service token
+    general_auth_url: URL = URL("https://www.googleapis.com/oauth2/v4/token")
+
+
+class GCPTokenStore:
+    def __init__(self, gcp_store_config: GCPTokenStoreConfig) -> None:
+
+        self._token_generation_time = -1
+        self._token_generated: Optional[str] = None
+
+        self._gcp_store_config = gcp_store_config
+
+        self._client = aiohttp.ClientSession()
+
+    async def close(self) -> None:
+        if self._client and not self._client.closed:
+            await self._client.close()
+
+    @classmethod
+    def __create_signed_jwt_key(
+        cls, key: str, iss: str, auth_url: str, issue_time: int, exp_time: int
+    ) -> str:
+        return jwt.encode(
+            headers={"alg": "RS256", "typ": "JWT"},
+            claims={
+                "iss": iss,
+                "scope": "https://www.googleapis.com/auth/logging.read",
+                "aud": auth_url,
+                "iat": issue_time,
+                "exp": exp_time,
+            },
+            key=key,
+            algorithm=constants.ALGORITHMS.RS256,
+        )
+
+    async def _request(self, url: URL, current_time: int) -> Optional[str]:
+        issue_time = current_time
+        exp_time = current_time + self._gcp_store_config.token_expiration
+        self._signed_jwt = GCPTokenStore.__create_signed_jwt_key(
+            self._gcp_store_config.private_key,
+            self._gcp_store_config.iss,
+            str(self._gcp_store_config.general_auth_url),
+            issue_time,
+            exp_time,
+        )
+        async with self._client.post(url, data=self._signed_jwt) as response:
+            if 400 <= response.status:
+                # TODO log here into the error log, and raise proper exception
+                return None
+            payload = await response.json()
+            return payload["access_token"]
+
+    async def get_token(self) -> Optional[str]:
+        import time
+
+        now = int(ceil(time.time()))
+        token_age = now - self._token_generation_time
+        if (not self._token_generated) or (
+            token_age > self._gcp_store_config.token_expiration
+        ):
+            # token would expire a bit earlier
+            token_value = await self._request(
+                self._gcp_store_config.general_auth_url, now
+            )
+            if token_value:
+                self._token_generation_time = now
+                self._token_generated = token_value
+        return self._token_generated
 
 
 @dataclass(frozen=True)
---
+++ tests/unit/integration/test_gcp_token_fetch.py
@@ -0,0 +1,126 @@
+from typing import Any
+from unittest import mock
+from unittest.mock import MagicMock, Mock
+
+import pytest
+
+from platform_api.config import GCPTokenStore, GCPTokenStoreConfig
+
+
+class MockResponse:
+    def __init__(self, code: int, payload: Any) -> None:
+        self._code = code
+        self._payload = payload
+
+    async def json(self) -> Any:
+        return self._payload
+
+    @property
+    def status(self) -> int:
+        return self._code
+
+
+class TestGCPStackDriverConf:
+    """
+    Test cases to ensure stack driver logging storage properly picked.
+    """
+
+    private_key = """-----BEGIN RSA PRIVATE KEY-----
+MIIBzAIBAAJhAL+pBqnLy6A7x0FWpDOKKO242q4sSjN37NazvdBCy3/TcySghY49
+yV1KbEkx/xDhUZQDlrUOcUiUvUkynjVRnY6MzPP1LzOZ4TUkSOQZeSF/7zGzpDMp
+QDq9PozliAVydQIDAQABAmEAu7cU66DK4zkpQvlVAZXD2HFA3W5LjnVa5j5NHnkN
+TzkOgUUnCdXCLzkBLf9lzmafCSSHQ0QOobEl+zjB6o8cj+gPh7vBHa8lTO6+eV+o
+fMJ/xYadU0EYPcApQQAZhKcBAjEA60eYaWwmIm0jxNayMlWALVCR5kfVLfTyV6Wy
+algD16UbUXQvjXDm7Zu2E51LFwgRAjEA0IoGyIimN/F86V0IuuN0aHm7dgeMG/Ia
+Obw/wP5wiGUnub5R0G7Wur5O/W69JMglAjEAyjVVKz3UpH/SXwj6S8IqEEgPrK+N
+6xp68ZMp/LW6T6rKCL5nZBNllU7fNIpaD+hRAjBdG9Ntg72bKsIXu4cjUlzuLLPr
+PfFrpMvazVc1xyTdcTSsdPI4etR66m+ALgpbOtUCMQChuZwmmaKcgYY7+tRvKoFG
+gUNwOdUjaxFd6quGJh8TAhRwgnHIOqjQ7Bau9SnZ3JE=
+-----END RSA PRIVATE KEY-----"""
+    config = GCPTokenStoreConfig(
+        private_key, iss="test-service@neuromation.io", token_expiration=10
+    )
+
+    @classmethod
+    def mocked_async_context_manager(cls, return_value=None):
+        class ContextManager:
+            def __init__(self, *args, **kwargs):
+                self._args = args
+
+            async def __aenter__(self):
+                return return_value
+
+            async def __aexit__(self, *args):
+                pass
+
+        return Mock(wraps=ContextManager)
+
+    @pytest.mark.asyncio
+    async def test_gcp_token(self) -> None:
+        def wrapper_async_call(mock):
+            async def internal(*args, **kwargs):
+                return mock()
+
+            return internal
+
+        store = GCPTokenStore(
+            GCPTokenStoreConfig(
+                private_key="private-key",
+                iss="test-service@neuromation.io",
+                token_expiration=10,
+            )
+        )
+
+        with mock.patch("time.time") as mock_time:
+            mock_time.return_value = -2
+            magic_mock = MagicMock()
+            store._request = wrapper_async_call(magic_mock)
+            await store.get_token()
+            assert magic_mock.call_count == 1
+
+            mock_time.return_value = 4
+            await store.get_token()
+            assert magic_mock.call_count == 1
+
+            mock_time.return_value = 5
+            await store.get_token()
+            assert magic_mock.call_count == 1
+
+            mock_time.return_value = 30
+            await store.get_token()
+            assert magic_mock.call_count == 2
+
+        await store.close()
+
+    @pytest.mark.asyncio
+    async def test_gcp_token_http(self) -> None:
+
+        store = GCPTokenStore(TestGCPStackDriverConf.config)
+
+        with mock.patch(
+            "aiohttp.ClientSession.request",
+            new=TestGCPStackDriverConf.mocked_async_context_manager(
+                MockResponse(400, None)
+            ),
+        ):
+            with mock.patch("time.time") as mock_time:
+                mock_time.return_value = 1000
+                assert await store.get_token() is None
+
+        await store.close()
+
+    @pytest.mark.asyncio
+    async def test_gcp_token_valid_response(self) -> None:
+        store = GCPTokenStore(TestGCPStackDriverConf.config)
+
+        with mock.patch(
+            "aiohttp.ClientSession.post",
+            new=TestGCPStackDriverConf.mocked_async_context_manager(
+                MockResponse(200, {"access_token": "token_test_value"})
+            ),
+        ):
+            with mock.patch("time.time") as mock_time:
+                mock_time.return_value = 1020
+                assert await store.get_token() == "token_test_value"
+
+        await store.close()
