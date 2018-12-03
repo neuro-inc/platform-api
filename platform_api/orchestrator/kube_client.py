a315794206547c5c8ac97720e17402a34925cbe0
Aleksandr Danshyn <dalazx@gmail.com>
2018-12-03 17:43:57 +0200

    Added network policies to KubeClient

--- platform_api/orchestrator/kube_client.py
+++ platform_api/orchestrator/kube_client.py
@@ -953,6 +953,10 @@ class KubeClient:
     def _api_v1_url(self) -> str:
         return f"{self._base_url}/api/v1"
 
+    @property
+    def _apis_networking_v1_url(self) -> str:
+        return f"{self._base_url}/apis/networking.k8s.io/v1"
+
     def _generate_namespace_url(self, namespace_name: str) -> str:
         return f"{self._api_v1_url}/namespaces/{namespace_name}"
 
@@ -966,6 +970,19 @@ class KubeClient:
 
     def _generate_pod_url(self, pod_id: str) -> str:
         return f"{self._pods_url}/{pod_id}"
+
+    def _generate_all_network_policies_url(
+        self, namespace_name: Optional[str] = None
+    ) -> str:
+        namespace_name = namespace_name or self._namespace
+        namespace_url = f"{self._apis_networking_v1_url}/namespaces/{namespace_name}"
+        return f"{namespace_url}/networkpolicies"
+
+    def _generate_network_policy_url(
+        self, name: str, namespace_name: Optional[str] = None
+    ) -> str:
+        all_nps_url = self._generate_all_network_policies_url(namespace_name)
+        return f"{all_nps_url}/{name}"
 
     @property
     def _v1beta1_namespace_url(self) -> str:
@@ -1189,3 +1206,69 @@ class KubeClient:
         if response.status != 200:
             payload = await response.text()
             raise KubeClientException(payload)
+
+    async def create_default_network_policy(
+        self,
+        name: str,
+        pod_labels: Dict[str, str],
+        namespace_name: Optional[str] = None,
+    ) -> Dict[str, Any]:
+        assert pod_labels
+        # TODO: comments
+        # https://tools.ietf.org/html/rfc1918#section-3
+        request_payload = {
+            "apiVersion": "networking.k8s.io/v1",
+            "kind": "NetworkPolicy",
+            "metadata": {"name": name},
+            "spec": {
+                "podSelector": {"matchLabels": pod_labels},
+                "policyTypes": ["Egress"],
+                "egress": [
+                    {
+                        "to": [
+                            {
+                                "ipBlock": {
+                                    "cidr": "0.0.0.0/0",
+                                    "except": [
+                                        "10.0.0.0/8",
+                                        "172.16.0.0/12",
+                                        "192.168.0.0/16",
+                                    ],
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "to": [
+                            {"ipBlock": {"cidr": "10.0.0.0/8"}},
+                            {"ipBlock": {"cidr": "172.16.0.0/12"}},
+                            {"ipBlock": {"cidr": "192.168.0.0/16"}},
+                        ],
+                        "ports": [
+                            {"port": 53, "protocol": "UDP"},
+                            {"port": 53, "protocol": "TCP"},
+                        ],
+                    },
+                    {"to": [{"podSelector": {"matchLabels": pod_labels}}]},
+                ],
+            },
+        }
+        url = self._generate_all_network_policies_url(namespace_name)
+        payload = await self._request(method="POST", url=url, json=request_payload)
+        self._check_status_payload(payload)
+        return payload
+
+    async def get_network_policy(
+        self, name: str, namespace_name: Optional[str] = None
+    ) -> Dict[str, Any]:
+        url = self._generate_network_policy_url(name, namespace_name)
+        payload = await self._request(method="GET", url=url)
+        self._check_status_payload(payload)
+        return payload
+
+    async def delete_network_policy(
+        self, name: str, namespace_name: Optional[str] = None
+    ) -> None:
+        url = self._generate_network_policy_url(name, namespace_name)
+        payload = await self._request(method="DELETE", url=url)
+        self._check_status_payload(payload)
