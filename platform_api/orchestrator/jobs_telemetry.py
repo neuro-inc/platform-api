50546ee2e864f8d49664bb8d7ad24015a2865b5b
kyryl <truskovskiyk@gmail.com>
2018-12-18 15:53:53 +0200

    add time; add abc class

--- platform_api/orchestrator/jobs_telemetry.py
+++ platform_api/orchestrator/jobs_telemetry.py
@@ -1,4 +1,6 @@
-from dataclasses import asdict, dataclass
+from abc import ABC, abstractmethod
+from dataclasses import asdict, dataclass, field
+import time
 from typing import Any, Dict
 
 
@@ -6,15 +8,23 @@ from typing import Any, Dict
 class JobTop:
     cpu: float
     mem: float
+    timestamp: float = field(default_factory=time.time)
 
     def to_primitive(self) -> Dict[str, Any]:
         return asdict(self)
 
 
-class DataDogClient:
+class TelemetryClient(ABC):
+
+    @abstractmethod
+    async def get_job_top(self, job_id: str) -> JobTop:
+        pass
+
+
+class DataDogClient(TelemetryClient):
     # TODO (truskovskiyk 17/12/18) implement it
     # https://github.com/neuromation/platform-api/issues/377
-    def get_job_top(self, job_id: str) -> JobTop:
+    async def get_job_top(self, job_id: str) -> JobTop:
         # TODO (truskovskiyk fetch from data dog)
         return JobTop(cpu=1, mem=16)
 
@@ -24,7 +34,7 @@ class JobsTelemetry:
         self._data_dog_client = data_dog_client
 
     async def get_job_top(self, job_id: str) -> JobTop:
-        job_top = self._data_dog_client.get_job_top(job_id)
+        job_top = await self._data_dog_client.get_job_top(job_id)
         return job_top
 
     @classmethod
--- tests/unit/test_jobs_telemetry.py
+++ tests/unit/test_jobs_telemetry.py
@@ -1,5 +1,5 @@
 import pytest
-
+import time
 from platform_api.orchestrator.jobs_telemetry import JobsTelemetry, JobTop
 
 
@@ -15,11 +15,17 @@ def jobs_telemetry():
 
 class TestJobTop:
     def test_job_top_to_primitive(self, job_top):
-        assert {"cpu": 1, "mem": 1} == job_top.to_primitive()
+        job_top_primitive = job_top.to_primitive()
+        del job_top_primitive['timestamp']
+        assert {"cpu": 1, "mem": 1} == job_top_primitive
+
+    def test_job_top_timestamp(self, job_top):
+        job_top_primitive = job_top.to_primitive()
+        assert job_top_primitive['timestamp'] < time.time()
 
 
 class TestJobsTelemetry:
     async def test_get_job_top(self, jobs_telemetry):
         job_id = "job_id"
-        job_top = jobs_telemetry.get_job_top(job_id=job_id)
+        job_top = await jobs_telemetry.get_job_top(job_id=job_id)
         assert {"cpu": 1, "mem": 1} == job_top.to_primitive()
