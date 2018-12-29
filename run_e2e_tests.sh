565bc083b653a03906fdf899811bb14e34f761df
Alex Davydow <adavydow@gmail.com>
2018-12-29 17:49:32 +0300

    e2e tests refactored. Some additional fixtures in conftest to simplif… (#392)
    * e2e tests refactored. Some additional fixtures in conftest to simplify testing in multiuser environment
    
    * missed file added
    
    * redundant type annotation removed
    
    * test for plathformauthapi service start too

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -o verbose
 docker tag $(cat AUTH_SERVER_IMAGE_NAME) platformauthapi:latest
 
 # NOTE (A Danshyn 10/04/18): in order to run this test locally using a VM-based
@@ -9,18 +9,37 @@ docker tag $(cat AUTH_SERVER_IMAGE_NAME) platformauthapi:latest
 # eval $(minikube docker-env)
 # docker load -i /tmp/platformauthapi.image
 # docker load -i /tmp/platformapi.image
+# kubectl delete -f deploy/platformapi/templates/rb.default.gke.yml
+# kubectl delete -f tests/k8s/platformapi.yml
 
 kubectl create -f deploy/platformapi/templates/rb.default.gke.yml
 kubectl create -f tests/k8s/platformapi.yml
 
+wait for uptime
+
+
+check_service() { # attempt, max_attempt, service
+    local attempt=$1
+    local max_attempts=$2
+    local service=$3
+    until minikube service platformapi --url; do
+	if [ $attempt == $max_attempts ]; then
+            exit 1
+	fi
+	sleep 1
+	((attempt++))
+    done    
+}
+
 attempt=1
 max_attempts=30
-until minikube service platformapi --url; do
-    if [ $attempt == $max_attempts ]; then
-        exit 1
-    fi
-    sleep 1
-    ((attempt++))
-done
 
-PLATFORM_API_URL=$(minikube service platformapi --url)/api/v1 make test_e2e
+check_service attempt max_attempts platformapi
+check_service attempt max_attempts platformauthapi
+
+# wait till everything is up to prevent flakes
+sleep 10
+
+export PLATFORM_API_URL=$(minikube service platformapi --url)/api/v1
+export AUTH_API_URL=$(minikube service platformauthapi --url)
+make test_e2e
