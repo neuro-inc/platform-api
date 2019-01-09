e4f31ce4411da79633c12db76153924c5bdf0273
Alex Davydow <adavydow@gmail.com>
2019-01-09 14:30:31 +0300

    script to run e2e tests fixed (#396)
    * script to run e2e tests fixed
    
    * uncomented two lines which were commented by mistake
    
    * Reading CI env variable, as frequent commenting/uncommenting leads to numerous errors
    
    * Redundant comment removed

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -2,28 +2,30 @@
 set -o verbose
 docker tag $(cat AUTH_SERVER_IMAGE_NAME) platformauthapi:latest
 
-# NOTE (A Danshyn 10/04/18): in order to run this test locally using a VM-based
-# minikube, one needs to uncomment these lines first
-# docker save -o /tmp/platformauthapi.image platformauthapi:latest
-# docker save -o /tmp/platformapi.image platformapi-k8s:latest
-# eval $(minikube docker-env)
-# docker load -i /tmp/platformauthapi.image
-# docker load -i /tmp/platformapi.image
-# kubectl delete -f deploy/platformapi/templates/rb.default.gke.yml
-# kubectl delete -f tests/k8s/platformapi.yml
+if [ ! "$CI" = true ]; then
+    echo "Setting up external services"
+    docker save -o /tmp/platformauthapi.image platformauthapi:latest
+    docker save -o /tmp/platformapi.image platformapi-k8s:latest
+    eval $(minikube docker-env)
+    docker load -i /tmp/platformauthapi.image
+    docker load -i /tmp/platformapi.image
+    kubectl delete -f deploy/platformapi/templates/rb.default.gke.yml
+    kubectl delete -f tests/k8s/platformapi.yml
+    echo "Services set up"
+fi
 
 kubectl create -f deploy/platformapi/templates/rb.default.gke.yml
 kubectl create -f tests/k8s/platformapi.yml
 
-wait for uptime
-
+# wait for containers to start
 
 check_service() { # attempt, max_attempt, service
-    local attempt=$1
-    local max_attempts=$2
-    local service=$3
+    local attempt=1
+    local max_attempts=$1
+    local service=$2
     until minikube service platformapi --url; do
 	if [ $attempt == $max_attempts ]; then
+	    echo "Can't connect to the container"
             exit 1
 	fi
 	sleep 1
@@ -31,13 +33,12 @@ check_service() { # attempt, max_attempt, service
     done    
 }
 
-attempt=1
 max_attempts=30
 
-check_service attempt max_attempts platformapi
-check_service attempt max_attempts platformauthapi
+check_service $max_attempts platformapi
+check_service $max_attempts platformauthapi
 
-# wait till everything is up to prevent flakes
+# wait till our services are up to prevent flakes
 sleep 10
 
 export PLATFORM_API_URL=$(minikube service platformapi --url)/api/v1
