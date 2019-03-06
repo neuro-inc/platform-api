8287220936d862baefa656a6b1313ae19b7a28f6
Aleksandr Danshyn <dalazx@gmail.com>
2019-03-06 18:03:39 +0200

    Integrated KubeOrchestrator

--- platform_api/orchestrator/job_request.py
+++ platform_api/orchestrator/job_request.py
@@ -100,14 +100,14 @@ class ContainerResources:
 class ContainerHTTPServer:
     port: int
     health_check_path: str = "/"
-    is_auth_required: bool = False
+    requires_auth: bool = False
 
     @classmethod
     def from_primitive(cls, payload) -> "ContainerHTTPServer":
         return cls(  # type: ignore
             port=payload["port"],
             health_check_path=payload.get("health_check_path") or cls.health_check_path,
-            is_auth_required=payload.get("is_auth_required", cls.is_auth_required),
+            requires_auth=payload.get("requires_auth", cls.requires_auth),
         )
 
     def to_primitive(self) -> Dict:
@@ -179,6 +179,10 @@ class Container:
     @property
     def has_ssh_server_exposed(self) -> bool:
         return bool(self.ssh_server)
+
+    @property
+    def requires_http_auth(self) -> bool:
+        return bool(self.http_server and self.http_server.requires_auth)
 
     @classmethod
     def from_primitive(cls, payload) -> "Container":
