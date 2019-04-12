10a2acc5eeade0a2e58199f2213f4d7103f28c13
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-04-12 14:33:22 +0300

    remove unnecessary __post_init

--- platform_api/handlers/jobs_handler.py
+++ platform_api/handlers/jobs_handler.py
@@ -451,10 +451,6 @@ class BulkJobFilter:
     shared_ids: Set[str]
     shared_ids_filter: JobFilter
 
-    def __post_init__(self):
-        if self.shared_ids:
-            assert self.shared_ids_filter is not None, f"no filter, {self.shared_ids}"
-
 
 class BulkJobFilterBuilder:
     def __init__(
