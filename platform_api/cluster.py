991b322f4b82a80f134205b012c79b1f7b9cdc7c
Aleksandr Danshyn <dalazx@gmail.com>
2019-04-17 18:29:15 +0300

    Implemented ClusterRegistry

---
+++ platform_api/cluster.py
@@ -0,0 +1,97 @@
+import logging
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from typing import AsyncIterator, Callable, Dict, Optional
+
+from aiorwlock import RWLock
+from async_generator import asynccontextmanager
+
+from .orchestrator import Orchestrator
+
+
+logger = logging.getLogger(__name__)
+
+
+class ClusterException(Exception):
+    pass
+
+
+class ClusterNotFound(ClusterException):
+    pass
+
+
+@dataclass(frozen=True)
+class ClusterConfig:
+    name: str
+
+
+class Cluster(ABC):
+    @abstractmethod
+    async def init(self) -> None:  # pragma: no cover
+        pass
+
+    @abstractmethod
+    async def close(self) -> None:  # pragma: no cover
+        pass
+
+    @property
+    @abstractmethod
+    def name(self) -> str:  # pragma: no cover
+        pass
+
+    @property
+    @abstractmethod
+    def orchestrator(self) -> Orchestrator:  # pragma: no cover
+        pass
+
+
+ClusterFactory = Callable[[ClusterConfig], Cluster]
+
+
+@dataclass(frozen=True)
+class ClusterRegistryRecord:
+    cluster: Cluster
+    lock: RWLock = field(default_factory=RWLock)
+
+    @property
+    def name(self) -> str:
+        return self.cluster.name
+
+
+class ClusterRegistry:
+    def __init__(self, *, factory: ClusterFactory) -> None:
+        self._factory = factory
+        self._records: Dict[str, ClusterRegistryRecord] = {}
+
+    def _add(self, record: ClusterRegistryRecord) -> None:
+        assert record.name not in self._records
+        self._records[record.name] = record
+
+    def _remove(self, name: str) -> Optional[ClusterRegistryRecord]:
+        return self._records.pop(name, None)
+
+    async def add(self, config: ClusterConfig) -> None:
+        await self.remove(config.name)
+
+        cluster = self._factory(config)
+        await cluster.init()
+
+        record = ClusterRegistryRecord(cluster=cluster)
+        self._add(record)
+
+    async def remove(self, name: str) -> None:
+        record = self._remove(name)
+        if not record:
+            return
+
+        async with record.lock.writer_lock:
+            await record.cluster.close()
+
+    @asynccontextmanager
+    async def get(self, name: str) -> AsyncIterator[Cluster]:
+        record = self._records.get(name)
+        if not record:
+            raise ClusterNotFound(f"Cluster '{name}' not found")
+
+        async with record.lock.reader_lock:
+            yield record.cluster
---
+++ tests/unit/test_cluster.py
@@ -0,0 +1,147 @@
+import asyncio
+
+import pytest
+from async_timeout import timeout
+
+from platform_api.cluster import (
+    Cluster,
+    ClusterConfig,
+    ClusterNotFound,
+    ClusterRegistry,
+)
+from platform_api.orchestrator import Orchestrator
+
+
+class _TestCluster(Cluster):
+    def __init__(self, config: ClusterConfig) -> None:
+        self._config = config
+
+    async def init(self) -> None:
+        pass
+
+    async def close(self) -> None:
+        pass
+
+    @property
+    def name(self) -> str:
+        return self._config.name
+
+    @property
+    def orchestrator(self) -> Orchestrator:
+        pass
+
+
+class TestClusterRegistry:
+    @pytest.mark.asyncio
+    async def test_get_not_found(self) -> None:
+        registry = ClusterRegistry(factory=_TestCluster)
+
+        with pytest.raises(ClusterNotFound, match="Cluster 'test' not found"):
+            async with registry.get("test"):
+                pass
+
+    @pytest.mark.asyncio
+    async def test_add(self) -> None:
+        registry = ClusterRegistry(factory=_TestCluster)
+        config = ClusterConfig(name="test")
+
+        await registry.add(config)
+
+        async with registry.get(config.name) as cluster:
+            assert cluster.name == config.name
+
+    @pytest.mark.asyncio
+    async def test_add_existing(self) -> None:
+        registry = ClusterRegistry(factory=_TestCluster)
+        name = "test"
+        config = ClusterConfig(name=name)
+
+        with pytest.raises(ClusterNotFound, match=f"Cluster '{name}' not found"):
+            async with registry.get(name):
+                pass
+
+        await registry.add(config)
+
+        old_cluster = None
+        async with registry.get(config.name) as cluster:
+            assert cluster.name == config.name
+            old_cluster = cluster
+
+        await registry.add(config)
+
+        new_cluster = None
+        async with registry.get(config.name) as cluster:
+            assert cluster.name == config.name
+            new_cluster = cluster
+
+        assert old_cluster is not new_cluster
+
+    @pytest.mark.asyncio
+    async def test_remove_missing(self) -> None:
+        registry = ClusterRegistry(factory=_TestCluster)
+        name = "test"
+
+        with pytest.raises(ClusterNotFound, match=f"Cluster '{name}' not found"):
+            async with registry.get(name):
+                pass
+
+        await registry.remove(name)
+
+        with pytest.raises(ClusterNotFound, match=f"Cluster '{name}' not found"):
+            async with registry.get(name):
+                pass
+
+    @pytest.mark.asyncio
+    async def test_remove(self) -> None:
+        registry = ClusterRegistry(factory=_TestCluster)
+        name = "test"
+        config = ClusterConfig(name=name)
+
+        with pytest.raises(ClusterNotFound, match=f"Cluster '{name}' not found"):
+            async with registry.get(name):
+                pass
+
+        await registry.add(config)
+
+        async with registry.get(config.name) as cluster:
+            assert cluster.name == config.name
+
+        await registry.remove(name)
+
+        with pytest.raises(ClusterNotFound, match=f"Cluster '{name}' not found"):
+            async with registry.get(name):
+                pass
+
+    @pytest.mark.asyncio
+    async def test_remove_locked(self, event_loop) -> None:
+        registry = ClusterRegistry(factory=_TestCluster)
+        name = "test"
+        config = ClusterConfig(name=name)
+
+        await registry.add(config)
+
+        async with registry.get(name):
+            with pytest.raises(asyncio.TimeoutError):
+                async with timeout(0.1):
+                    await event_loop.create_task(registry.remove(name))
+
+    @pytest.mark.asyncio
+    async def test_remove_another_locked(self, event_loop) -> None:
+        registry = ClusterRegistry(factory=_TestCluster)
+        config = ClusterConfig(name="test1")
+        anotherconfig = ClusterConfig(name="test2")
+
+        await registry.add(config)
+        await registry.add(anotherconfig)
+
+        async with registry.get(config.name):
+            async with registry.get(anotherconfig.name):
+                pass
+
+            await registry.remove(anotherconfig.name)
+
+            with pytest.raises(
+                ClusterNotFound, match=f"Cluster '{anotherconfig.name}' not found"
+            ):
+                async with registry.get(anotherconfig.name):
+                    pass
