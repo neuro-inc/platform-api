f5e11c4f54dfc394fdb9d438f3b22b0b6b4cde6f
Aleksandr Danshyn <dalazx@gmail.com>
2019-04-23 22:24:48 +0300

    Added KubeCluster

---
+++ platform_api/kube_cluster.py
@@ -0,0 +1,59 @@
+import logging
+from typing import Optional
+
+from aioelasticsearch import Elasticsearch
+from async_exit_stack import AsyncExitStack
+
+from .cluster import Cluster
+from .cluster_config import ClusterConfig
+from .elasticsearch import create_elasticsearch_client
+from .orchestrator import KubeConfig, KubeOrchestrator, Orchestrator
+
+
+logger = logging.getLogger(__name__)
+
+
+class KubeCluster(Cluster):
+    def __init__(self, config: ClusterConfig) -> None:
+        self._config = config
+
+        self._exit_stack = AsyncExitStack()
+
+        self._es_client: Optional[Elasticsearch] = None
+        self._orchestrator: Optional[Orchestrator] = None
+
+    @property
+    def name(self) -> str:
+        return self._config.name
+
+    @property
+    def orchestrator(self) -> Orchestrator:
+        assert self._orchestrator
+        return self._orchestrator
+
+    async def init(self) -> None:
+        await self._exit_stack.__aenter__()
+        await self._init_es_client()
+        await self._init_orchestrator()
+
+    async def _init_es_client(self) -> None:
+        assert not self._es_client
+        logger.info(f"Cluster '{self.name}': initializing Elasticsearch client")
+        es_client = await self._exit_stack.enter_async_context(
+            create_elasticsearch_client(self._config.logging.elasticsearch)
+        )
+        self._es_client = es_client
+
+    async def _init_orchestrator(self) -> None:
+        assert self._es_client
+        assert not self._orchestrator
+        assert isinstance(self._config, KubeConfig)
+        logger.info(f"Cluster '{self.name}': initializing Orchestrator")
+        orchestrator = KubeOrchestrator(
+            config=self._config.orchestrator, es_client=self._es_client
+        )
+        await self._exit_stack.enter_async_context(orchestrator)
+        self._orchestrator = orchestrator
+
+    async def close(self) -> None:
+        await self._exit_stack.__aexit__()
