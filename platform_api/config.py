1b919112a6a440957ee7ccc59fe9f38075b7a5bd
Aleksandr Danshyn <dalazx@gmail.com>
2019-05-10 15:27:35 -0400

    Removed ClusterConfig from Config

--- platform_api/config.py
+++ platform_api/config.py
@@ -5,14 +5,7 @@ from typing import Container, Optional, Sequence
 
 from yarl import URL
 
-from .cluster_config import (
-    ClusterConfig,
-    IngressConfig,
-    LoggingConfig,
-    OrchestratorConfig,
-    RegistryConfig,
-    StorageConfig,
-)
+from .cluster_config import OrchestratorConfig, RegistryConfig, StorageConfig
 from .redis import RedisConfig
 
 
@@ -72,8 +65,6 @@ class JobsConfig:
 class Config:
     server: ServerConfig
 
-    cluster: ClusterConfig
-
     database: DatabaseConfig
     auth: AuthConfig
     oauth: Optional[OAuthConfig] = None
@@ -83,26 +74,6 @@ class Config:
     # used for generating environment variable names and
     # sourcing them inside containers.
     env_prefix: str = "NP"  # stands for Neuromation Platform
-
-    @property
-    def storage(self) -> StorageConfig:
-        return self.cluster.storage
-
-    @property
-    def registry(self) -> RegistryConfig:
-        return self.cluster.registry
-
-    @property
-    def orchestrator(self) -> OrchestratorConfig:
-        return self.cluster.orchestrator
-
-    @property
-    def logging(self) -> LoggingConfig:
-        return self.cluster.logging
-
-    @property
-    def ingress(self) -> IngressConfig:
-        return self.cluster.ingress
 
 
 @dataclass(frozen=True)
