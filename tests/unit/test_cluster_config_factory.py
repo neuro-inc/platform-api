24f49d84aacab7cbcce0b25daae2a128d155fae7
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-05-15 14:51:08 -0400

    fix tests after rebase

--- tests/unit/test_cluster_config_factory.py
+++ tests/unit/test_cluster_config_factory.py
@@ -116,10 +116,11 @@ class TestClusterConfigFactory:
 
         storage = cluster.storage
         assert storage.type == StorageType.NFS
-        nfs_mount_point = PurePath(storage_payload["nfs"]["export_path"])
-        assert storage.host_mount_path == nfs_mount_point
+        assert storage.host_mount_path is None
         assert storage.nfs_server == storage_payload["nfs"]["server"]
-        assert storage.nfs_export_path == nfs_mount_point
+        assert storage.nfs_export_path == PurePath(
+            storage_payload["nfs"]["export_path"]
+        )
 
         orchestrator = cluster.orchestrator
         assert isinstance(orchestrator, KubeConfig)
@@ -192,7 +193,7 @@ class TestClusterConfigFactory:
         storage = cluster.storage
         assert storage.type == StorageType.NFS
         nfs_mount_point = PurePath(storage_payload["nfs"]["export_path"])
-        assert storage.host_mount_path == nfs_mount_point
+        assert storage.host_mount_path is None
         assert storage.nfs_server == storage_payload["nfs"]["server"]
         assert storage.nfs_export_path == nfs_mount_point
 
@@ -218,7 +219,7 @@ class TestClusterConfigFactory:
         # initialized fields:
         assert config.nfs_server == "127.0.0.1"
         assert config.nfs_export_path == PurePath("/nfs/export/path")
-        assert config.host_mount_path == PurePath("/nfs/export/path")
+        assert config.host_mount_path is None
         assert config.type == StorageType.NFS
         # default fields:
         assert config.container_mount_path == PurePath("/var/storage")
