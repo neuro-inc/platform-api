aa13fb70514cd1d03b37b9f0d56625af8b5e7c1f
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-08-22 12:58:08 +0300

    fix 2

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -289,7 +289,7 @@ class TestJobs:
             "container": {
                 "image": f"registry.dev.neuromation.io/anotheruser/image:tag",
                 "command": "true",
-                "missing": {"cpu": 0.1, "memory_mb": 16},
+                "resources": {"cpu": 0.1, "memory_mb": 16},
             }
         }
 
