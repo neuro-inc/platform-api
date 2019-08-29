a55a5d8c960820c4bd59a5cfd7a7821b5afabc3b
Aleksandr Danshyn <dalazx@gmail.com>
2019-08-29 16:22:14 +0300

    Fixed the issue with unneeded required env var for SSH-auth (#857)
    * Fixed the issue with unneeded required env var for SSH-auth
    
    * Addressed

--- platform_api/config_factory.py
+++ platform_api/config_factory.py
@@ -244,7 +244,7 @@ class EnvironConfigFactory:
         url = URL(self._environ["NP_AUTH_URL"])
         token = self._environ["NP_AUTH_TOKEN"]
         name = self._environ.get("NP_AUTH_NAME", AuthConfig.service_name)
-        public_endpoint_url = URL(self._environ["NP_AUTH_PUBLIC_URL"])
+        public_endpoint_url = URL(self._environ.get("NP_AUTH_PUBLIC_URL", ""))
         return AuthConfig(
             server_endpoint_url=url,
             service_token=token,
