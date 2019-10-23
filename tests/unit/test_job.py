2dc7ec60692f7127a9a986916c421d541afe8b5f
Artem Yushkovskiy <ajuszkowski@ya.ru>
2019-10-23 18:29:44 +0300

    Add field `max_run_time_minutes` (#922)

--- tests/unit/test_job.py
+++ tests/unit/test_job.py
@@ -738,6 +738,38 @@ class TestJob:
             "schedule_timeout": 15,
         }
 
+    def test_to_primitive_with_max_run_time(
+        self, mock_orchestrator: MockOrchestrator, job_request: JobRequest
+    ) -> None:
+        job = Job(
+            storage_config=mock_orchestrator.storage_config,
+            orchestrator_config=mock_orchestrator.config,
+            record=JobRecord.create(
+                request=job_request,
+                cluster_name="test-cluster",
+                max_run_time_minutes=500,
+            ),
+        )
+        assert job.to_primitive() == {
+            "id": "testjob",
+            "owner": "compute",
+            "cluster_name": "test-cluster",
+            "request": job_request.to_primitive(),
+            "status": "pending",
+            "statuses": [
+                {
+                    "status": "pending",
+                    "transition_time": mock.ANY,
+                    "reason": None,
+                    "description": None,
+                }
+            ],
+            "is_deleted": False,
+            "finished_at": None,
+            "is_preemptible": False,
+            "max_run_time_minutes": 500,
+        }
+
     def test_from_primitive(
         self, mock_orchestrator: MockOrchestrator, job_request_payload: Dict[str, Any]
     ) -> None:
@@ -760,6 +792,7 @@ class TestJob:
         assert job.name is None
         assert job.owner == "testuser"
         assert not job.is_preemptible
+        assert job.max_run_time == timedelta.max
 
     def test_from_primitive_check_name(
         self, mock_orchestrator: MockOrchestrator, job_request_payload: Dict[str, Any]
@@ -907,6 +940,84 @@ class TestJob:
         )
         assert job.request.container.command == "arg1 arg2 arg3"
         assert job.request.container.entrypoint == "/script.sh"
+
+    def test_from_primitive_with_max_run_time_minutes(
+        self, mock_orchestrator: MockOrchestrator, job_request_payload: Dict[str, Any]
+    ) -> None:
+        payload = {
+            "id": "testjob",
+            "name": "test-job-name",
+            "owner": "testuser",
+            "request": job_request_payload,
+            "status": "succeeded",
+            "is_deleted": True,
+            "finished_at": datetime.now(timezone.utc).isoformat(),
+            "max_run_time_minutes": 100,
+        }
+        job = Job.from_primitive(
+            mock_orchestrator.storage_config, mock_orchestrator.config, payload
+        )
+        assert job.max_run_time == timedelta(minutes=100)
+
+    def test_from_primitive_with_max_run_time_minutes_none(
+        self, mock_orchestrator: MockOrchestrator, job_request_payload: Dict[str, Any]
+    ) -> None:
+        payload = {
+            "id": "testjob",
+            "name": "test-job-name",
+            "owner": "testuser",
+            "request": job_request_payload,
+            "status": "succeeded",
+            "is_deleted": True,
+            "finished_at": datetime.now(timezone.utc).isoformat(),
+            "max_run_time_minutes": None,
+        }
+        job = Job.from_primitive(
+            mock_orchestrator.storage_config, mock_orchestrator.config, payload
+        )
+        assert job.max_run_time == timedelta.max
+
+    def test_from_primitive_with_max_run_time_minutes_zero(
+        self, mock_orchestrator: MockOrchestrator, job_request_payload: Dict[str, Any]
+    ) -> None:
+        payload = {
+            "id": "testjob",
+            "name": "test-job-name",
+            "owner": "testuser",
+            "request": job_request_payload,
+            "status": "succeeded",
+            "is_deleted": True,
+            "finished_at": datetime.now(timezone.utc).isoformat(),
+            "max_run_time_minutes": 0,
+        }
+        job = Job.from_primitive(
+            mock_orchestrator.storage_config, mock_orchestrator.config, payload
+        )
+        with pytest.raises(
+            AssertionError, match="max_run_time_minutes must be positive, got: 0"
+        ):
+            job.max_run_time
+
+    def test_from_primitive_with_max_run_time_minutes_negative(
+        self, mock_orchestrator: MockOrchestrator, job_request_payload: Dict[str, Any]
+    ) -> None:
+        payload = {
+            "id": "testjob",
+            "name": "test-job-name",
+            "owner": "testuser",
+            "request": job_request_payload,
+            "status": "succeeded",
+            "is_deleted": True,
+            "finished_at": datetime.now(timezone.utc).isoformat(),
+            "max_run_time_minutes": -1,
+        }
+        job = Job.from_primitive(
+            mock_orchestrator.storage_config, mock_orchestrator.config, payload
+        )
+        with pytest.raises(
+            AssertionError, match="max_run_time_minutes must be positive, got: -1"
+        ):
+            job.max_run_time
 
     def test_to_uri(
         self, mock_orchestrator: MockOrchestrator, job_request: JobRequest
@@ -1365,6 +1476,26 @@ class TestAggregatedRunTime:
             total_non_gpu_run_time_delta=timedelta.max,
         )
         assert run_time.to_primitive() == {"total_gpu_run_minutes": 30}
+
+    def test_to_primitive_gpu_zero(self) -> None:
+        run_time = AggregatedRunTime(
+            total_gpu_run_time_delta=timedelta(minutes=0),
+            total_non_gpu_run_time_delta=timedelta(minutes=60),
+        )
+        assert run_time.to_primitive() == {
+            "total_gpu_run_minutes": 0,
+            "total_non_gpu_run_minutes": 60,
+        }
+
+    def test_to_primitive_non_gpu_zero(self) -> None:
+        run_time = AggregatedRunTime(
+            total_gpu_run_time_delta=timedelta(minutes=30),
+            total_non_gpu_run_time_delta=timedelta(minutes=0),
+        )
+        assert run_time.to_primitive() == {
+            "total_gpu_run_minutes": 30,
+            "total_non_gpu_run_minutes": 0,
+        }
 
 
 class TestTimeDeltaConverter:
