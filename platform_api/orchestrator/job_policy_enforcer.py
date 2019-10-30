07a798ec82856af4303eec7f3aa22f13bfb0443f
Alexey Naiden <alexey.nayden@gmail.com>
2019-10-30 13:49:27 +0200

    Fixes

--- platform_api/orchestrator/job_policy_enforcer.py
+++ platform_api/orchestrator/job_policy_enforcer.py
@@ -1,12 +1,57 @@
 import abc
-import asyncio
 import logging
-from typing import Any, Optional
+from typing import Any, Dict, Set
+
+import aiohttp
 
 from platform_api.config import JobPolicyEnforcerConfig
+from platform_api.orchestrator.job import AggregatedRunTime
+from platform_api.orchestrator.job_request import JobStatus
 
 
 logger = logging.getLogger(__name__)
+
+
+class JobPolicyEnforcerClientWrapper:
+    @abc.abstractmethod
+    async def get_users_with_active_jobs(self) -> Dict[Any, Any]:
+        pass
+
+    @abc.abstractmethod
+    async def get_user_stats(self, username: str) -> Dict[Any, Any]:
+        pass
+
+    @abc.abstractmethod
+    async def kill_job(self, job_id: str) -> None:
+        pass
+
+
+class RealJobPolicyEnforcerClientWrapper(JobPolicyEnforcerClientWrapper):
+    def __init__(self, config: JobPolicyEnforcerConfig):
+        self._platform_api_url = config.platform_api_url
+        self._headers = {"Authorization": f"Bearer {config.token}"}
+        self._session = aiohttp.ClientSession(headers=self._headers)
+
+    async def get_users_with_active_jobs(self) -> Dict[Any, Any]:
+        async with self._session.get(
+            self._platform_api_url / "jobs?status=pending&status=running",
+            headers=self._headers,
+        ) as resp:
+            resp.raise_for_status()
+            return await resp.json()
+
+    async def get_user_stats(self, username: str) -> Dict[Any, Any]:
+        async with self._session.get(
+            self._platform_api_url / f"stats/user/{username}", headers=self._headers
+        ) as resp:
+            resp.raise_for_status()
+            return await resp.json()
+
+    async def kill_job(self, job_id: str) -> None:
+        async with self._session.delete(
+            self._platform_api_url / f"jobs/{job_id}", headers=self._headers
+        ) as resp:
+            resp.raise_for_status()
 
 
 class JobPolicyEnforcer:
@@ -15,48 +60,57 @@ class JobPolicyEnforcer:
         pass
 
 
-class JobPolicyEnforcePoller:
-    def __init__(
-        self, policy_enforcer: JobPolicyEnforcer, config: JobPolicyEnforcerConfig
+class QuotaJobPolicyEnforcer(JobPolicyEnforcer):
+    def __init__(self, wrapper: RealJobPolicyEnforcerClientWrapper):
+        self._wrapper = wrapper
+
+    async def enforce(self) -> None:
+        users_with_active_jobs = await self.get_users_with_active_jobs()
+        for user, job_ids in users_with_active_jobs.items():
+            await self.check_user_quota(user, job_ids["cpu"], job_ids["gpu"])
+
+    async def get_users_with_active_jobs(self) -> Dict[str, Dict[str, Set[str]]]:
+        response_payload = await self._wrapper.get_users_with_active_jobs()
+        jobs = response_payload["jobs"]
+        jobs_by_owner: Dict[str, Dict[str, Set[str]]] = {}
+        for job in jobs:
+            job_status = JobStatus(response_payload["status"])
+            if job_status.is_running or job_status.is_pending:
+                owner = job.get("owner")
+                if owner:
+                    existing_jobs = jobs_by_owner.get(
+                        owner, {"gpu": set(), "cpu": set()}
+                    )
+                    is_gpu = job["container"]["resources"].get("gpu") is not None
+                    if is_gpu:
+                        existing_jobs["gpu"].add(job["id"])
+                    else:
+                        existing_jobs["cpu"].add(job["id"])
+                    jobs_by_owner[owner] = existing_jobs
+
+        return jobs_by_owner
+
+    async def check_user_quota(
+        self, username: str, cpu_job_ids: Set[str], gpu_job_ids: Set[str]
     ) -> None:
-        self._loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
-        self._policy_enforcer = policy_enforcer
-        self._config = config
+        response_payload = await self._wrapper.get_user_stats(username)
+        quota = AggregatedRunTime.from_primitive(response_payload["quota"])
+        jobs = AggregatedRunTime.from_primitive(response_payload["jobs"])
 
-        self._task: Optional[asyncio.Task[None]] = None
+        assert quota is not None
+        assert jobs is not None
 
-    async def __aenter__(self) -> "JobPolicyEnforcePoller":
-        await self.start()
-        return self
+        jobs_to_delete: Set[str] = set()
+        if quota.total_non_gpu_run_time_delta < jobs.total_non_gpu_run_time_delta:
+            logger.info(f"CPU quota exceeded for {username}")
+            jobs_to_delete = cpu_job_ids.union(gpu_job_ids)
+        elif quota.total_gpu_run_time_delta < jobs.total_gpu_run_time_delta:
+            logger.info(f"GPU quota exceeded for {username}")
+            jobs_to_delete = gpu_job_ids
+        else:
+            logger.info(f"No quota issues for {username}")
 
-    async def __aexit__(self, *args: Any) -> None:
-        await self.stop()
-
-    async def start(self) -> None:
-        logger.info("Starting job policy enforce polling")
-        assert self._task is None
-        self._task = self._loop.create_task(self._run())
-
-    async def stop(self) -> None:
-        logger.info("Stopping job policy enforce polling")
-        assert self._task is not None
-        self._task.cancel()
-        self._task = None
-
-    async def _run(self) -> None:
-        while True:
-            start = self._loop.time()
-            await self._run_once()
-            elapsed = self._loop.time() - start
-            delay = self._config.interval_sec - elapsed
-            if delay < 0:
-                delay = 0
-            await asyncio.sleep(delay)
-
-    async def _run_once(self) -> None:
-        try:
-            await self._policy_enforcer.enforce()
-        except asyncio.CancelledError:
-            raise
-        except BaseException:
-            logger.exception("Exception when trying to enforce jobs policies")
+        if len(jobs_to_delete) > 0:
+            logger.info(f"Killing jobs: {jobs_to_delete}")
+            for job_id in jobs_to_delete:
+                await self._wrapper.kill_job(job_id)
