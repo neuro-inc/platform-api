269da7f07f4c8d148f6b86f3279f4dd866c2bc4b
Aleksandr Danshyn <dalazx@gmail.com>
2019-11-26 15:22:29 +0200

    Attempt to fix a flaky test #1013 (#1015)

--- tests/integration/test_kube_orchestrator.py
+++ tests/integration/test_kube_orchestrator.py
@@ -5,6 +5,7 @@ import uuid
 from dataclasses import replace
 from pathlib import PurePath
 from typing import Any, AsyncIterator, Awaitable, Callable, Dict, Iterator, Optional
+from unittest import mock
 
 import aiohttp
 import pytest
@@ -1653,7 +1654,7 @@ class TestPodContainerDevShmSettings:
             status=JobStatus.FAILED,
             reason=JobStatusReason.OOM_KILLED,
             exit_code=137,
-            description=None,
+            description=mock.ANY,
         )
         assert status_actual == status_expected, f"actual: '{status_actual}'"
 
