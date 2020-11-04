ae102695f0440bedf24a30b25415fe2b25540a07
Ivan Zubenko <vanya.zubenko@gmail.com>
2020-11-04 12:36:12 +0200

    fix dependencies

--- setup.py
+++ setup.py
@@ -14,8 +14,6 @@ install_requires = (
     "aiorwlock==1.0.0",
     "notifications-client==0.8.2",
     "platform-logging==0.3",
-    "yarl==1.5.1",
-    "multidict==4.7.6",
     "aiohttp-cors==0.7.0",
     "aiozipkin==0.7.1",
     "asyncpg==0.21.0",
