cf9a280695bb7f0c7497d9f2afefaddfc8f9329b
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-06-16 11:47:05 +0300

    Bump trafaret version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup_requires = ("setuptools_scm",)
 install_requires = (
     "aiohttp==3.7.4.post0",
     "iso8601==0.1.14",
-    "trafaret==1.2.0",
+    "trafaret==2.1.0",
     "neuro_auth_client==21.5.17",
     # Circle CI fails on the latest cryptography version
     # because the server has too old OpenSSL version
