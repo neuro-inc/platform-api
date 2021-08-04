d6e1e0c87816d65573a490679883a05a873649fb
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-08-04 14:16:05 +0300

    Pin neuro-logging==21.8.4.1 (#1648)
    Committed via https://github.com/asottile/all-repos

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = (
     "cryptography==3.3.2",
     "aiorwlock==1.0.0",
     "notifications-client==21.5.17.post1",
-    "neuro-logging==21.8.4",
+    "neuro-logging==21.8.4.1",
     "aiohttp-cors==0.7.0",
     "aiozipkin==1.1.0",
     "asyncpg==0.23.0",
