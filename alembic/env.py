ea812593da9c6e6a00facb1fffada961185cf584
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-09-15 12:43:37 +0300

    fix

--- alembic/env.py
+++ alembic/env.py
@@ -9,7 +9,7 @@ from platform_api.config_factory import EnvironConfigFactory
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-config = context.config
+config = context.config  # type: ignore[attr-defined]
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
