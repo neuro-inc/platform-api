54262a004e1a80823d3302083688e3f3f68e939e
Serhiy Storchaka <storchaka@gmail.com>
2021-11-09 10:40:51 +0200

    Refactor test_postgres_available.

--- tests/integration/test_postgres.py
+++ tests/integration/test_postgres.py
@@ -18,7 +18,7 @@ async def test_postgres_available(sqalchemy_engine: AsyncEngine) -> None:
     async with sqalchemy_engine.connect() as connection:
         result = await connection.execute(sa.text("SELECT 2 + 2;"))
         row = result.one()
-        assert row[0] == 4
+        assert row == (4,)
 
 
 @pytest.mark.asyncio
