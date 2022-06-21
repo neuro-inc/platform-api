c187805406630f7641243f591cccb1444b17fac4
Roman Skurikhin <romasku135@gmail.com>
2022-06-21 12:44:36 +0300

    Use power of 2 for memory_mb

--- platform_api/handlers/job_request_builder.py
+++ platform_api/handlers/job_request_builder.py
@@ -70,7 +70,7 @@ def create_resources_from_payload(payload: dict[str, Any]) -> ContainerResources
         cpu=payload["cpu"],
         memory=payload["memory"]
         if "memory" in payload
-        else payload["memory_mb"] * 10**6,
+        else payload["memory_mb"] * 2**20,
         gpu=payload.get("gpu"),
         gpu_model_id=payload.get("gpu_model"),
         shm=payload.get("shm"),
