258e628db5f51f9053d4dea4f586c964e76f330f
Roman Skurikhin <romasku135@gmail.com>
2022-06-24 11:58:43 +0300

    Properly pass notifications to admin in tests

--- tests/integration/admin.py
+++ tests/integration/admin.py
@@ -119,7 +119,7 @@ async def _admin_server_setup_db(
             f"NP_ADMIN_CONFIG_TOKEN={admin_token}",
             "NP_ADMIN_CONFIG_URL=http://host.docker.internal:8089",
             f"NP_ADMIN_POSTGRES_DSN={admin_postgres_dsn}",
-            f"NP_ADMIN_NOTIFICATIONS_URL={mock_notifications_server.url}",
+            f"NP_ADMIN_NOTIFICATIONS_URL=http://host.docker.internal:8083",
             "NP_ADMIN_NOTIFICATIONS_TOKEN=token",
         ],
     }
@@ -170,7 +170,7 @@ async def admin_server(
             f"NP_ADMIN_CONFIG_TOKEN={admin_token}",
             "NP_ADMIN_CONFIG_URL=http://host.docker.internal:8089",
             f"NP_ADMIN_POSTGRES_DSN={admin_postgres_dsn}",
-            f"NP_ADMIN_NOTIFICATIONS_URL={mock_notifications_server.url}",
+            f"NP_ADMIN_NOTIFICATIONS_URL=http://host.docker.internal:8083",
             "NP_ADMIN_NOTIFICATIONS_TOKEN=token",
         ],
     }
