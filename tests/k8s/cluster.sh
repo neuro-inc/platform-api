7d4191963e4a735650564cafb05f983c7b8c2c93
Ivan Zubenko <vanya.zubenko@gmail.com>
2025-05-27 11:59:11 +0200

    fix

--- tests/k8s/cluster.sh
+++ tests/k8s/cluster.sh
@@ -15,12 +15,10 @@ function k8s::start {
     touch $KUBECONFIG
 
     export MINIKUBE_WANTUPDATENOTIFICATION=false
-    export MINIKUBE_WANTREPORTERRORPROMPT=false
     export MINIKUBE_HOME=$HOME
     export CHANGE_MINIKUBE_NONE_USER=true
 
     sudo -E mkdir -p ~/.minikube/files/files
-    sudo -E minikube config set WantReportErrorPrompt false
     sudo -E minikube config set WantUpdateNotification false
     sudo -E minikube config set WantNoneDriverWarning false
 
