45821afab5c26baec5a97ce09387bfcc7994ab17
Andrew Svetlov <andrew.svetlov@gmail.com>
2025-11-21 17:49:50 +0100

    fix

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -517,7 +517,9 @@ async def delete_node_later(
 @pytest.fixture
 async def kube_node(kube_client_selector: KubeClientSelector) -> str:
     nodes = await kube_client_selector.host_client.core_v1.node.get_list()
-    return nodes[0].metadata.name
+    name = nodes.items[0].metadata.name
+    assert name is not None
+    return name
 
 
 @pytest.fixture
